608a933d5390526f19f05ae12b68fbf6a7dabd6b
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 22:59:22 +0530

    Added service to persist `request logs` of the `user`

---
+++ spec/services/request_logs/create_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/request_logs/create_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe RequestLogs::CreateService, type: :service do
+end
