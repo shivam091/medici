9bef7b59e77cd15a6db9b285899ce4437ac7b9ef
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 22:57:19 +0530

    Added required associations in `User` and `RequestLog` models

--- app/models/request_log.rb
+++ app/models/request_log.rb
@@ -3,4 +3,11 @@
 # -*- warn_indent: true -*-
 
 class RequestLog < ApplicationRecord
+  include Sortable, Filterable, UpcaseAttribute
+
+  upcase_attributes! :method
+
+  belongs_to :user, optional: true
+
+  default_scope -> { order_created_desc }
 end
