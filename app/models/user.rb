c96f6f786015d8fe44ea35d88e8e7944e1cab1a0
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 21:06:38 +0530

    Added required `scopes` and `finders` for authenticating `user`

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,6 @@ class User < ApplicationRecord
   attribute :is_active, default: false
   attribute :password_automatically_set, default: false
 
-  belongs_to :role
-
   validates :login, presence: true, if: :login_required
   validates :password,
             presence: true,
@@ -37,4 +35,72 @@ class User < ApplicationRecord
             presence: true,
             reduce: true,
             if: proc { password.present? }
+
+  belongs_to :role
+
+  class << self
+    def with_email_or_mobile_number(login)
+      iwhere(mobile_number: login.strip).or(iwhere(email: login.strip)).first
+    end
+
+    def find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      login = conditions.delete(:login)
+      where(conditions).with_email_or_mobile_number(login)
+    end
+
+    def with_email(email)
+      iwhere(email: email).first
+    end
+
+    def send_reset_password_instructions(attributes = {})
+      recoverable = find_or_initialize_recoverable_with_errors(reset_password_keys, attributes, :not_found)
+      recoverable.send_reset_password_instructions if recoverable.persisted?
+      recoverable
+    end
+
+    def find_or_initialize_recoverable_with_errors(required_attributes, attributes, error = :invalid)
+      attributes = attributes.slice(*required_attributes)
+      login = attributes[:login]
+      record = with_email_or_mobile_number(login)
+
+      unless record
+        record = new
+        required_attributes.each do |key|
+          value = attributes[key]
+          record.send("#{key}=", value)
+          if value.present?
+            record.errors.add(key, error)
+          else
+            record.errors.add(key, :blank)
+          end
+        end
+      end
+
+      record.login = login
+      record
+    end
+  end
+
+  def full_name
+    "#{try(:first_name)} #{try(:last_name)}"
+  end
+
+  def recently_sent_password_reset_instructions?
+    reset_password_sent_at.present? && reset_password_sent_at >= THROTTLE_RESET_PERIOD.minute.ago
+  end
+
+  def active_for_authentication?
+    super && is_active?
+  end
+
+  private
+
+  def validate_login
+    login_required
+  end
+
+  def validate_password
+    password_required
+  end
 end
