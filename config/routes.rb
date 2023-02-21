fa26b59a6eff04f5e76ef506b02e32f234c10092
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 20:48:37 +0530

    Added `routes` and `module` containing helpers for rendering `favicon`

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@
 # -*- warn_indent: true -*-
 
 Rails.application.routes.draw do
+  favicon_redirect = redirect do |_params, _request|
+    ActionController::Base.helpers.asset_url(::Medici::Favicon.main)
+  end
 
+  get "favicon.png", to: favicon_redirect
+  get "favicon.ico", to: favicon_redirect
 end
