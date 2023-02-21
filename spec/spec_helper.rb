ec49d7095994105e041a514c5143910572167a50
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 16:41:30 +0530

    Configured `shoulda-matchers`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ require_relative "../config/environment"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require "rspec/rails"
+require "shoulda/matchers"
 require "rspec-parameterized"
 
 def spec_root
