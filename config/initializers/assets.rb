331265effa3efef7dc23e5f4ee9c02f5c28bb5ac
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 16:22:32 +0530

    Configured `assets`

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,10 +7,17 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+Rails.application.config.action_controller.asset_host = Rails.application.credentials.config[:DOMAIN]
+
+Rails.application.config.assets.css_compressor = :sass
+
+Rails.application.config.assets.debug = true
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(
+  .svg
+  bootstrap.min.js
+  popper.js
+)
