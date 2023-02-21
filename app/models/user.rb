b3949c5259c6395fa450398232479aaaddf3be53
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 21:02:20 +0530

    Added model `User` along with required `attributes`

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/models/user_spec.rb
+
+require "spec_helper"
+
+RSpec.describe User, type: :model do
+end
