a931073591aa043a9709f46478f111d366fad1f6
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 16:19:47 +0530

    Configured `pagy`, `secret token`, and `server uptime`

---
+++ config/initializers/server_uptime.rb
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# Be sure to restart your server when you modify this file.
+
+Medici::Application.config.booted_at = Time.now
