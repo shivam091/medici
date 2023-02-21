8c0147d2c647088c43064b408b0f085f67ab5984
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-21 20:57:15 +0530

    Added required `validations` in `Role` model

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,13 @@
 # -*- warn_indent: true -*-
 
 class Role < ApplicationRecord
+  include Filterable
+
+  attribute :is_active, default: false
+
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: {maximum: 55},
+            reduce: true
 end
