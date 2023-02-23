1ef79f051b10c00ba012eca7c8d8c3ba5bd99f92
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 19:08:36 +0530

    Added feature to `create` the `ingredient`

--- app/controllers/admin/ingredients_controller.rb
+++ app/controllers/admin/ingredients_controller.rb
@@ -9,4 +9,39 @@ class Admin::IngredientsController < Admin::BaseController
     @ingredients = ::Ingredient.active
     @pagy, @ingredients = pagy(@ingredients)
   end
+
+  # GET /admin/ingredients/new
+  def new
+    @ingredient = ::Ingredient.new
+  end
+
+  # POST /admin/ingredients
+  def create
+    response = ::Ingredients::CreateService.(ingredient_params)
+    @ingredient = response.payload[:ingredient]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_ingredients_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:ingredient_form, partial: "admin/ingredients/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(
+      :name,
+      :description,
+      :is_active,
+    )
+  end
 end
