7b2a56e87343b5bdc756659a81eef55c0e5a6304
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 19:40:31 +0530

    Added listing page for `dosage forms`

--- app/controllers/admin/dosage_forms_controller.rb
+++ app/controllers/admin/dosage_forms_controller.rb
@@ -3,4 +3,10 @@
 # -*- warn_indent: true -*-
 
 class Admin::DosageFormsController < Admin::BaseController
+
+  # GET /admin/dosage-forms
+  def index
+    @dosage_forms = ::DosageForm.active.includes(:currency)
+    @pagy, @dosage_forms = pagy(@dosage_forms)
+  end
 end
