b3f43b706a242e1612266d7b4949ad044a2dd865
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 13:03:35 +0530

    Added required translations for `currencies` section

---
+++ config/locales/en/flash_messages.rb
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+{
+  en: {
+    flash_messages: {
+      currencies: {
+        create: {
+          success: "Currency '%{currency_name}' was successfully created.",
+          error: "Currency could not be created.",
+        },
+        update: {
+          success: "Currency '%{currency_name}' was successfully updated.",
+          error: "Currency could not be updated.",
+        },
+        destroy: {
+          success: "Currency '%{currency_name}' was successfully destroyed.",
+          error: "Currency '%{currency_name}' could not be destroyed.",
+        }
+      }
+    }
+  }
+}
