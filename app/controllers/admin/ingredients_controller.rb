6e28c84e63a3f44ecde85eb685083c03f8688ba7
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 19:10:13 +0530

    Added feaure to `update` the `ingredient`

--- app/controllers/admin/ingredients_controller.rb
+++ app/controllers/admin/ingredients_controller.rb
@@ -4,6 +4,8 @@
 
 class Admin::IngredientsController < Admin::BaseController
 
+  before_action :find_ingredient, except: [:index, :new, :create]
+
   # GET /admin/ingredients
   def index
     @ingredients = ::Ingredient.active
@@ -35,6 +37,30 @@ class Admin::IngredientsController < Admin::BaseController
     end
   end
 
+  # GET /admin/ingredients/:uuid/edit
+  def edit
+  end
+
+  # PUT/PATCH /admin/ingredients/:uuid
+  def update
+    response = ::Ingredients::UpdateService.(@ingredient, ingredient_params)
+    @ingredient = response.payload[:ingredient]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_ingredients_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:ingredient_form, partial: "admin/ingredients/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
   private
 
   def ingredient_params
@@ -43,5 +69,9 @@ class Admin::IngredientsController < Admin::BaseController
       :description,
       :is_active,
     )
+  end
+
+  def find_ingredient
+    @ingredient = ::Ingredient.find(params.fetch(:uuid))
   end
 end
