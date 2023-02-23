b9b794f92eaf08112fa19f3da6b85f33f4798da2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 19:53:45 +0530

    Added feature to `update` the `dosage form`

--- app/controllers/admin/dosage_forms_controller.rb
+++ app/controllers/admin/dosage_forms_controller.rb
@@ -4,6 +4,8 @@
 
 class Admin::DosageFormsController < Admin::BaseController
 
+  before_action :find_dosage_form, except: [:index, :new, :create]
+
   # GET /admin/dosage-forms
   def index
     @dosage_forms = ::DosageForm.active
@@ -35,6 +37,30 @@ class Admin::DosageFormsController < Admin::BaseController
     end
   end
 
+  # GET /admin/dosage-forms/:uuid/edit
+  def edit
+  end
+
+  # PUT/PATCH /admin/dosage-forms/:uuid
+  def update
+    response = ::DosageForms::UpdateService.(@dosage_form, dosage_form_params)
+    @dosage_form = response.payload[:dosage_form]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_dosage_forms_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:dosage_form_form, partial: "admin/dosage_forms/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
   private
 
   def dosage_form_params
@@ -42,5 +68,9 @@ class Admin::DosageFormsController < Admin::BaseController
       :name,
       :is_active,
     )
+  end
+
+  def find_dosage_form
+    @dosage_form = ::DosageForm.find(params.fetch(:uuid))
   end
 end
