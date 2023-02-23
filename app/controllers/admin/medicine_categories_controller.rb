fee32680ad306ad43f69f76a1b909f8642ecc907
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 20:19:11 +0530

    Added feature to `create` the `medicine category`

--- app/controllers/admin/medicine_categories_controller.rb
+++ app/controllers/admin/medicine_categories_controller.rb
@@ -4,9 +4,44 @@
 
 class Admin::MedicineCategoriesController < Admin::BaseController
 
-# GET /admin/medicine-categories
-def index
-  @medicine_categories = ::MedicineCategory.active
-  @pagy, @medicine_categories = pagy(@medicine_categories)
-end
+  # GET /admin/medicine-categories
+  def index
+    @medicine_categories = ::MedicineCategory.active
+    @pagy, @medicine_categories = pagy(@medicine_categories)
+  end
+
+  # GET /admin/medicine-categories/new
+  def new
+    @medicine_category = ::MedicineCategory.new
+  end
+
+  # POST /admin/medicine-categories
+  def create
+    response = ::MedicineCategories::CreateService.(medicine_category_params)
+    @medicine_category = response.payload[:medicine_category]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_medicine_categories_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:medicine_category_form, partial: "admin/medicine_categories/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def medicine_category_params
+    params.require(:medicine_category).permit(
+      :name,
+      :description,
+      :is_active,
+    )
+  end
 end
