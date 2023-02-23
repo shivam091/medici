52496953102ab047f6c169f84f656eac4f24764e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 12:57:26 +0530

    Added service to `create` the `currency`

---
+++ spec/services/currencies/create_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/currencies/create_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Currencies::CreateService, type: :service do
+end
