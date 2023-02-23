38ca18604a591ee1c4989e251cd8997485e88588
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 21:34:46 +0530

    Added service to `destroy` the `medicine category`

---
+++ spec/services/medicine_categories/destroy_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/medicine_categories/destroy_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe MedicineCategories::DestroyService, type: :service do
+end
