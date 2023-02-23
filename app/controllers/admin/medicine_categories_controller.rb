2847a18b7eefff44199eca2d9304940da8f45969
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 20:15:40 +0530

    Added listing page for `medicine categories`

--- app/controllers/admin/medicine_categories_controller.rb
+++ app/controllers/admin/medicine_categories_controller.rb
@@ -3,4 +3,10 @@
 # -*- warn_indent: true -*-
 
 class Admin::MedicineCategoriesController < Admin::BaseController
+
+# GET /admin/medicine-categories
+def index
+  @medicine_categories = ::MedicineCategory.active
+  @pagy, @medicine_categories = pagy(@medicine_categories)
+end
 end
