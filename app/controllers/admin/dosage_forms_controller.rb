7c64cb1c9d228c399dcd4cfa0852373d5afda5e3
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 19:48:59 +0530

    Added feature to `create` the `dosage form`

--- app/controllers/admin/dosage_forms_controller.rb
+++ app/controllers/admin/dosage_forms_controller.rb
@@ -6,7 +6,41 @@ class Admin::DosageFormsController < Admin::BaseController
 
   # GET /admin/dosage-forms
   def index
-    @dosage_forms = ::DosageForm.active.includes(:currency)
+    @dosage_forms = ::DosageForm.active
     @pagy, @dosage_forms = pagy(@dosage_forms)
+  end
+
+  # GET /admin/dosage-forms/new
+  def new
+    @dosage_form = ::DosageForm.new
+  end
+
+  # POST /admin/dosage-forms
+  def create
+    response = ::DosageForms::CreateService.(dosage_form_params)
+    @dosage_form = response.payload[:dosage_form]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_dosage_forms_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:dosage_form_form, partial: "admin/dosage_forms/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def dosage_form_params
+    params.require(:dosage_form).permit(
+      :name,
+      :is_active,
+    )
   end
 end
