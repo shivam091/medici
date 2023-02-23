01aa5656454bfe20e90400ede136ddcfcf109af6
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 19:10:18 +0530

    Added service to `destroy` the `ingredient`

---
+++ spec/services/ingredients/destroy_service_spec.rb
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/ingredients/destroy_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Ingredients::DestroyService, type: :service do
+  context "doing something" do
+    it "should test something" do
+      skip
+    end
+  end
+end
