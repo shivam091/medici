992340030a94eced45d43dcb49ca23afd22a3f69
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-23 19:03:48 +0530

    Added listing page for `ingredients`

--- app/controllers/admin/ingredients_controller.rb
+++ app/controllers/admin/ingredients_controller.rb
@@ -3,4 +3,10 @@
 # -*- warn_indent: true -*-
 
 class Admin::IngredientsController < Admin::BaseController
+
+  # GET /admin/ingredients
+  def index
+    @ingredients = ::Ingredient.active
+    @pagy, @ingredients = pagy(@ingredients)
+  end
 end
