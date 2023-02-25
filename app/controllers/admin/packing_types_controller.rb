11c21e04ad75dbffb5b2323cd253a96093bb4701
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-25 22:27:22 +0530

    Added feature to `update` the `packing type`

--- app/controllers/admin/packing_types_controller.rb
+++ app/controllers/admin/packing_types_controller.rb
@@ -4,6 +4,8 @@
 
 class Admin::PackingTypesController < Admin::BaseController
 
+  before_action :find_packing_type, except: [:index, :new, :create]
+
   # GET /admin/packing-types
   def index
     @packing_types = ::PackingType.active
@@ -35,9 +37,37 @@ class Admin::PackingTypesController < Admin::BaseController
     end
   end
 
+  # GET /admin/packing-types/:uuid/edit
+  def edit
+  end
+
+  # PUT/PATCH /admin/packing-types/:uuid
+  def update
+    response = ::PackingTypes::UpdateService.(@packing_type, packing_type_params)
+    @packing_type = response.payload[:packing_type]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_packing_types_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:packing_type_form, partial: "admin/packing_types/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
   private
 
   def packing_type_params
     params.require(:packing_type).permit(:name, :is_active)
+  end
+
+  def find_packing_type
+    @packing_type = ::PackingType.find(params.fetch(:uuid))
   end
 end
