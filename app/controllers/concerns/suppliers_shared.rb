cabf3d0411adbbbd4e443b07174607a985ab5e2e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-25 15:53:26 +0530

    Added feature to `create` the `supplier`

--- app/controllers/concerns/suppliers_shared.rb
+++ app/controllers/concerns/suppliers_shared.rb
@@ -13,6 +13,50 @@ module SuppliersShared
         @suppliers = ::Supplier.active.includes(:address)
         @pagy, @suppliers = pagy(@suppliers)
       end
+
+      # GET /(admin|manager)/suppliers/new
+      def new
+        @supplier = ::Supplier.new
+      end
+
+      # POST /(admin|manager)/suppliers
+      def create
+        response = ::Suppliers::CreateService.(supplier_params)
+        @supplier = response.payload[:supplier]
+        if response.success?
+          flash[:notice] = response.message
+          redirect_to helpers.suppliers_path
+        else
+          flash.now[:alert] = response.message
+          respond_to do |format|
+            format.turbo_stream do
+              render turbo_stream: [
+                turbo_stream.update(:supplier_form, partial: "admin/suppliers/form"),
+                render_flash
+              ], status: :unprocessable_entity
+            end
+          end
+        end
+      end
     end
+  end
+
+  private
+
+  def supplier_params
+    params.require(:supplier).permit(
+      :name,
+      :email,
+      :phone_number,
+      :is_active,
+      address_attributes: [
+        :address1,
+        :address2,
+        :city,
+        :region,
+        :country_id,
+        :postal_code
+      ]
+    )
   end
 end
