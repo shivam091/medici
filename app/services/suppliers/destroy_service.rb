1626daaad7596a871734bad28c8f7a2ba1d42d3d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-25 16:22:36 +0530

    Added service to `destroy` the `supplier`

---
+++ app/services/suppliers/destroy_service.rb
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class Suppliers::DestroyService < ApplicationService
+  def initialize(supplier)
+    @supplier = supplier
+  end
+
+  def call
+    destroy_supplier
+  end
+
+  private
+
+  attr_reader :supplier
+
+  def destroy_supplier
+    if supplier.destroy
+      ::ServiceResponse.success(
+        message: t("suppliers.destroy.success", supplier_name: supplier.name),
+        payload: {supplier: supplier}
+      )
+    else
+      ::ServiceResponse.error(
+        message: t("suppliers.destroy.success", supplier_name: supplier.name),
+        payload: {supplier: supplier}
+      )
+    end
+  end
+end
