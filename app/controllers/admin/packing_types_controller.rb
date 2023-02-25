a8d006d2d453ac0c701c77ffb293e7fa0aa7888b
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-25 22:25:53 +0530

    Added feature to `create` the `packing type`

--- app/controllers/admin/packing_types_controller.rb
+++ app/controllers/admin/packing_types_controller.rb
@@ -9,4 +9,35 @@ class Admin::PackingTypesController < Admin::BaseController
     @packing_types = ::PackingType.active
     @pagy, @packing_types = pagy(@packing_types)
   end
+
+  # GET /admin/packing-types/new
+  def new
+    @packing_type = ::PackingType.new
+  end
+
+  # POST /admin/packing-types
+  def create
+    response = ::PackingTypes::CreateService.(packing_type_params)
+    @packing_type = response.payload[:packing_type]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_packing_types_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:packing_type_form, partial: "admin/packing_types/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def packing_type_params
+    params.require(:packing_type).permit(:name, :is_active)
+  end
 end
