92c22af18dd3b34736a08a5df03d7d243def48b7
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-26 21:04:50 +0530

    Added required `associations` in `Medicine` and `Supplier`

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -7,6 +7,12 @@ class Medicine < ApplicationRecord
 
   attribute :is_active, default: false
 
+  has_many :medicine_suppliers, dependent: :destroy
+  has_many :suppliers,
+           through: :medicine_suppliers,
+           source: :supplier,
+           inverse_of: :medicine_suppliers
+
   belongs_to :manufacturer, inverse_of: :medicines
   belongs_to :medicine_category, inverse_of: :medicines
   belongs_to :packing_type, inverse_of: :medicines
--- app/models/medicine_supplier.rb
+++ app/models/medicine_supplier.rb
@@ -3,4 +3,10 @@
 # -*- warn_indent: true -*-
 
 class MedicineSupplier < ApplicationRecord
+  attribute :total_quantity_supplied, default: 0
+
+  belongs_to :medicine, inverse_of: :medicine_suppliers
+  belongs_to :supplier, inverse_of: :medicine_suppliers
+
+  delegate :name, :email, :phone_number, to: :supplier, prefix: true
 end
