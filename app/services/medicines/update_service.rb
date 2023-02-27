e5cc961905796773e9917046316e474fb570d49d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-27 11:56:28 +0530

    Added service to `update` the `medicine`

---
+++ app/services/medicines/update_service.rb
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class Medicines::UpdateService < ApplicationService
+  def initialize(medicine, medicine_attributes)
+    @medicine = medicine
+    @medicine_attributes = medicine_attributes.dup
+  end
+
+  def call
+    update_medicine
+  end
+
+  private
+
+  attr_reader :medicine, :medicine_attributes
+
+  def update_medicine
+    if medicine.update(medicine_attributes)
+      ::ServiceResponse.success(
+        message: t("medicines.update.success", medicine_name: medicine.name, medicine_code: medicine.code),
+        payload: {medicine: medicine}
+      )
+    else
+      ::ServiceResponse.error(
+        message: t("medicines.update.error"),
+        payload: {medicine: medicine}
+      )
+    end
+  end
+end
