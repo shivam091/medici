e346fade1e3de7ee48f073df3f7a3e4508b143a8
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-28 18:19:05 +0530

    Added specs for `LowercaseValidator`

--- spec/validators/lowercase_validator_spec.rb
+++ spec/validators/lowercase_validator_spec.rb
@@ -7,4 +7,35 @@
 require "spec_helper"
 
 RSpec.describe LowercaseValidator do
+  using RSpec::Parameterized::TableSyntax
+
+  subject do
+    Class.new do
+      include ActiveModel::Model
+      include ActiveModel::Validations
+      attr_accessor :abbreviation
+
+      validates :abbreviation, lowercase: true
+
+      def self.model_name
+        ActiveModel::Name.new(self, nil, "LowercaseTestClass")
+      end
+    end.new
+  end
+
+  where(:abbreviation, :is_valid) do
+    "abc"       | true
+    "aaa"       | true
+    "Abc"       | false
+    "ABC"       | false
+    "abC"       | false
+  end
+
+  with_them do
+    it "only accepts lowercase characters" do
+      subject.abbreviation = abbreviation
+
+      expect(subject.valid?).to eq(is_valid)
+    end
+  end
 end
