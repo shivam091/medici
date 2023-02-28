dccccc64640b4fd19c4abf9a20a3285b61d6d43f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-02-28 19:53:57 +0530

    Added `validator` specs in context

--- spec/validators/email_validator_spec.rb
+++ spec/validators/email_validator_spec.rb
@@ -9,33 +9,35 @@ require "spec_helper"
 RSpec.describe EmailValidator do
   using RSpec::Parameterized::TableSyntax
 
-  subject do
-    Class.new do
-      include ActiveModel::Model
-      include ActiveModel::Validations
-      attr_accessor :email
+  context "when custom error message is not provided" do
+    subject do
+      Class.new do
+        include ActiveModel::Model
+        include ActiveModel::Validations
+        attr_accessor :email
 
-      validates :email, email: true
+        validates :email, email: true
 
-      def self.model_name
-        ActiveModel::Name.new(self, nil, "EmailValidatorTestClass")
+        def self.model_name
+          ActiveModel::Name.new(self, nil, "EmailValidatorTestClass")
+        end
+      end.new
+    end
+
+    where(:email, :is_valid) do
+      "admin@medici.com"       | true
+      "admin@medici.co.uk"     | true
+      "Abc"                    | false
+      "ABC"                    | false
+      "abC"                    | false
+    end
+
+    with_them do
+      it "only accepts valid email" do
+        subject.email = email
+
+        expect(subject.valid?).to eq(is_valid)
       end
-    end.new
-  end
-
-  where(:email, :is_valid) do
-    "admin@medici.com"       | true
-    "admin@medici.co.uk"     | true
-    "Abc"                    | false
-    "ABC"                    | false
-    "abC"                    | false
-  end
-
-  with_them do
-    it "only accepts valid email" do
-      subject.email = email
-
-      expect(subject.valid?).to eq(is_valid)
     end
   end
 end
--- spec/validators/lowercase_validator_spec.rb
+++ spec/validators/lowercase_validator_spec.rb
@@ -9,33 +9,35 @@ require "spec_helper"
 RSpec.describe LowercaseValidator do
   using RSpec::Parameterized::TableSyntax
 
-  subject do
-    Class.new do
-      include ActiveModel::Model
-      include ActiveModel::Validations
-      attr_accessor :abbreviation
+  context "when custom error message is not provided" do
+    subject do
+      Class.new do
+        include ActiveModel::Model
+        include ActiveModel::Validations
+        attr_accessor :abbreviation
 
-      validates :abbreviation, lowercase: true
+        validates :abbreviation, lowercase: true
 
-      def self.model_name
-        ActiveModel::Name.new(self, nil, "LowercaseTestClass")
+        def self.model_name
+          ActiveModel::Name.new(self, nil, "LowercaseTestClass")
+        end
+      end.new
+    end
+
+    where(:abbreviation, :is_valid) do
+      "abc"       | true
+      "aaa"       | true
+      "Abc"       | false
+      "ABC"       | false
+      "abC"       | false
+    end
+
+    with_them do
+      it "only accepts lowercase characters" do
+        subject.abbreviation = abbreviation
+
+        expect(subject.valid?).to eq(is_valid)
       end
-    end.new
-  end
-
-  where(:abbreviation, :is_valid) do
-    "abc"       | true
-    "aaa"       | true
-    "Abc"       | false
-    "ABC"       | false
-    "abC"       | false
-  end
-
-  with_them do
-    it "only accepts lowercase characters" do
-      subject.abbreviation = abbreviation
-
-      expect(subject.valid?).to eq(is_valid)
     end
   end
 end
