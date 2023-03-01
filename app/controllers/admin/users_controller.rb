21918acc0f7d516b1b3544c1dc0eae0f5266a71b
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-01 07:58:55 +0530

    Added listing page for `users`

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,10 @@
 # -*- warn_indent: true -*-
 
 class Admin::UsersController < Admin::BaseController
+
+  # GET /admin/users
+  def index
+    @users = ::User.active.includes(:role, :store)
+    @pagy, @users = pagy(@users)
+  end
 end
