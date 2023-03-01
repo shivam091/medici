51ab30c42d7bce3ec674a89a0f5942fc441fe5fa
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-01 22:18:54 +0530

    Added specs for `Ingredients::CreateService`

--- spec/services/ingredients/create_service_spec.rb
+++ spec/services/ingredients/create_service_spec.rb
@@ -7,4 +7,32 @@
 require "spec_helper"
 
 RSpec.describe Ingredients::CreateService, type: :service do
+  let(:ingredient_attributes) { attributes_for(:ingredient) }
+  subject { described_class.(ingredient_attributes) }
+
+  describe "#call" do
+    context "when ingredient is valid" do
+      it "returns a success response with the ingredient" do
+        expect(subject).to be_a(ServiceResponse)
+        expect(subject).to be_success
+        expect(subject.message).to eq("Ingredient '#{subject.payload[:ingredient].name}' was successfully created.")
+        expect(subject.payload).to include(ingredient: instance_of(::Ingredient))
+      end
+
+      include_examples "creates a new object", ::Ingredient
+    end
+
+    context "when ingredient is invalid" do
+      let(:ingredient_attributes) { attributes_for(:ingredient, name: "") }
+
+      it "returns an error response with the ingredient" do
+        expect(subject).to be_a(ServiceResponse)
+        expect(subject).to be_error
+        expect(subject.message).to eq("Ingredient could not be created.")
+        expect(subject.payload).to include(ingredient: instance_of(::Ingredient))
+      end
+
+      include_examples "does not change count of objects", ::Ingredient
+    end
+  end
 end
