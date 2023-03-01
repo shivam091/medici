2964173f268e2eee4d2ee7a9b4cb4ba746400d42
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-01 08:37:03 +0530

    Added feature to `create` the `user`

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,4 +9,51 @@ class Admin::UsersController < Admin::BaseController
     @users = ::User.active.includes(:role, :store)
     @pagy, @users = pagy(@users)
   end
+
+  # GET /admin/users/new
+  def new
+    @user = ::User.new
+  end
+
+  # POST /admin/users
+  def create
+    response = ::Users::CreateService.(user_params)
+    @user = response.payload[:user]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:user_form, partial: "admin/users/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :mobile_number,
+      :role_id,
+      :store_id,
+      :is_active,
+      address_attributes: [
+        :address1,
+        :address2,
+        :city,
+        :region,
+        :country_id,
+        :postal_code
+      ]
+    )
+  end
 end
