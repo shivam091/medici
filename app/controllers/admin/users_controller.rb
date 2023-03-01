7ca7ee4a10c666cf75255c94582c8f1bc33718b2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-01 10:22:16 +0530

    Added feature to `update` the `user`

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,8 @@
 
 class Admin::UsersController < Admin::BaseController
 
+  before_action :find_user, except: [:index, :new, :create]
+
   # GET /admin/users
   def index
     @users = ::User.active.includes(:role, :store)
@@ -18,6 +20,30 @@ class Admin::UsersController < Admin::BaseController
   # POST /admin/users
   def create
     response = ::Users::CreateService.(user_params)
+    @user = response.payload[:user]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:user_form, partial: "admin/users/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  # GET /admin/users/:uuid/edit
+  def edit
+  end
+
+  # PUT/PATCH /admin/users/:uuid
+  def update
+    response = ::Users::UpdateService.(@user, user_params)
     @user = response.payload[:user]
     if response.success?
       flash[:notice] = response.message
@@ -55,5 +81,9 @@ class Admin::UsersController < Admin::BaseController
         :postal_code
       ]
     )
+  end
+
+  def find_user
+    @user = ::User.find(params.fetch(:uuid))
   end
 end
