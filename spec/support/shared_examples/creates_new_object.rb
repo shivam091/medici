a6360f6d4bb03a68134901dadc67b2558c985420
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-01 22:13:07 +0530

    Added shared example for `create_new_object`

---
+++ spec/support/shared_examples/creates_new_object.rb
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+RSpec.shared_examples "creates a new object" do |klass|
+  it "creates a new #{klass.model_name.human.downcase}" do
+    expect { subject }.to change { klass.count }.by(1)
+  end
+end
