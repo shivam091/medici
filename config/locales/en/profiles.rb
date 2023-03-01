6ab9522ecf816e127414693d9cd171d562c680c7
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-01 15:02:36 +0530

    Added translations required for `profile` section

---
+++ config/locales/en/profiles.rb
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+{
+  en: {
+    profiles: {
+      show: {
+        title: "Your profile",
+        your_profile: "Your profile",
+        edit_profile_details: "Edit your profile details viz. first name, last name, etc.",
+        name: "Name",
+        mobile_number: "Mobile number",
+        email: "Email address",
+        password: "Password",
+        mobile_number_help_text: "Quickly sign in, easily recover passwords, and receive security notifications with this mobile number.",
+        login_and_security_preferences: "Login & security preferences",
+        login_and_security_preferences_help_text: "Edit mobile number or login settings",
+      },
+      edit: {
+        title: "Edit profile",
+      },
+      form: {
+        profile_details: "Profile details",
+        address: "Address",
+      }
+    }
+  }
+}
