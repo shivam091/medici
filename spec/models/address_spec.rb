75a989fae32b1d7481dadbd734e34c1d3694874c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-04 19:22:33 +0530

    Added specs for `Address` model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -7,4 +7,57 @@
 require "spec_helper"
 
 RSpec.describe Address, type: :model do
+  describe "valid factory" do
+    let(:addressable) { create(:admin, :confirmed) }
+    it { is_expected.to have_a_valid_factory.with_associations(addressable:) }
+  end
+
+  describe "superclasses" do
+    it { expect(described_class.ancestors).to include ApplicationRecord }
+  end
+
+  describe "included modules" do
+    it { is_expected.to include_module(Sortable) }
+  end
+
+  describe "delegates" do
+    it { is_expected.to delegate_method(:name).to(:country).with_prefix(true) }
+    it { is_expected.to delegate_method(:iso2).to(:country).with_prefix(true) }
+    it { is_expected.to delegate_method(:iso3).to(:country).with_prefix(true) }
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:addressable).optional }
+    it { is_expected.to belong_to(:country).inverse_of(:addresses) }
+  end
+
+  describe "validations" do
+    describe "#address1" do
+      it { is_expected.to validate_presence_of(:address1).with_message("is required") }
+      it { is_expected.to validate_length_of(:address1).is_at_most(100).with_message("is too long (maximum is 100 characters)") }
+    end
+
+    describe "#address2" do
+      it { is_expected.to validate_length_of(:address2).is_at_most(100).allow_nil.allow_blank.with_message("is too long (maximum is 100 characters)") }
+    end
+
+    describe "#city" do
+      it { is_expected.to validate_presence_of(:city).with_message("is required") }
+      it { is_expected.to validate_length_of(:city).is_at_most(100).with_message("is too long (maximum is 100 characters)") }
+    end
+
+    describe "#region" do
+      it { is_expected.to validate_length_of(:region).is_at_most(100).allow_nil.allow_blank.with_message("is too long (maximum is 100 characters)") }
+    end
+
+    describe "#postal_code" do
+      it { is_expected.to validate_length_of(:postal_code).is_at_most(20).allow_nil.allow_blank.with_message("is too long (maximum is 20 characters)") }
+    end
+
+    describe "#country_id" do
+      it { is_expected.to validate_presence_of(:country_id).with_message("is required") }
+    end
+  end
+
+  include_examples "apply default scope on created_at desc"
 end
