5c0903155d961687b8b9a47c7651f39c04c15cb0
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-04 19:17:05 +0530

    Added shared example for default scope of `created_at`

---
+++ spec/support/shared_examples/apply_default_scope_on_created_at_desc.rb
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+RSpec.shared_examples "apply default scope on created_at desc" do |object_name, klass|
+  describe "default scope" do
+    it "should apply default scope on #created_at" do
+      expect(described_class.all.to_sql).to eq(described_class.order(described_class.arel_table[:created_at].desc).to_sql)
+    end
+  end
+end
