e7c3fb068691d6da5c1a45fd2c2333aabc910ca4
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-04 18:08:17 +0530

    Added specs for `MedicineIngredient` model

--- spec/factories/medicine_ingredients.rb
+++ spec/factories/medicine_ingredients.rb
@@ -6,8 +6,11 @@ FactoryBot.define do
   factory :medicine_ingredient do
     association :medicine
     association(:ingredient, :active)
-    active { true }
     strength { 27.5 }
     uom { "mcg" }
+
+    trait :active do
+      active { true }
+    end
   end
 end
