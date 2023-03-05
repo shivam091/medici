02ddd7a8fc1f4fa97b6c8b9f88e8ec3e4443d571
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-05 12:32:00 +0530

    Added `AdminPolicy` and `IngredientPolicy`

---
+++ spec/policies/admin_policy_spec.rb
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/admin_policy_spec.rb
+
+RSpec.describe AdminPolicy, type: :policy do
+end
---
+++ spec/policies/ingredient_policy_spec.rb
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/ingredient_policy_spec.rb
+
+RSpec.describe IngredientPolicy, type: :policy do
+end
