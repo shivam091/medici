8fe3bf35a9baa05e652d45db1a577b4d46f8bec2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-08 13:50:57 +0530

    Added model `Customer` along with required `attributes`

---
+++ db/migrate/20230308064016_create_customers.rb
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class CreateCustomers < Medici::Database::Migration[1.0]
+  def change
+    create_table_with_constraints :customers, id: :uuid do |t|
+      t.string :reference_code
+      t.string :name
+      t.string :email, index: {using: :btree, unique: true}
+      t.string :mobile_number, index: {using: :btree, unique: true}
+      t.boolean :is_active, default: false
+
+      t.not_null_and_empty_constraint :reference_code
+      t.not_null_and_empty_constraint :name
+      t.not_null_and_empty_constraint :mobile_number
+
+      t.length_constraint :reference_code, less_than_or_equal_to: 15
+      t.length_constraint :name, less_than_or_equal_to: 110
+      t.length_constraint :email, less_than_or_equal_to: 55
+      t.length_constraint :mobile_number, less_than_or_equal_to: 32
+
+      t.timestamps_with_timezone null: false
+    end
+  end
+end
