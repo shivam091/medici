61a2351e5c3fbf3cc6362d8578686b167449f8d9
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-08 18:28:48 +0530

    Configured Simplecov `formatters` and `filters`

--- spec/simplecov_env.rb
+++ spec/simplecov_env.rb
@@ -9,12 +9,43 @@ module SimpleCovEnv
 
   def start!
     configure_profile
+    configure_formatter
+    configure_filters
+    write_coverage_percentage
 
     SimpleCov.start
   end
 
+  private
+
+  def configure_formatter
+    SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+      [
+        SimpleCov::Formatter::SimpleFormatter,
+        SimpleCov::Formatter::HTMLFormatter,
+      ]
+    )
+  end
+
+  def write_coverage_percentage
+    SimpleCov.at_exit do
+      File.open(File.join(SimpleCov.coverage_path, "coverage_percent.txt"), "w") do |f|
+        f.write SimpleCov.result.covered_percent
+      end
+      SimpleCov.result.format!
+    end
+  end
+
+  def configure_filters
+    SimpleCov.configure do
+      add_filter "spec"
+    end
+  end
+
   def configure_profile
     SimpleCov.configure do
+      load_profile "test_frameworks"
+
       add_group "Channels", "app/channels"
       add_group "Controllers", "app/controllers"
       add_group "Helpers", "app/helpers"
@@ -27,7 +58,7 @@ module SimpleCovEnv
       add_group "Workers", %w[app/jobs app/workers]
       add_group "Initializers", "config/initializers"
       add_group "Migrations", "db/migrate"
-      add_group "Libraries", "/lib"
+      add_group "Libraries", "lib"
 
       add_group "Long files" do |src_file|
         src_file.lines.count > 100
@@ -35,6 +66,8 @@ module SimpleCovEnv
       add_group "Short files" do |src_file|
         src_file.lines.count < 5
       end
+
+      merge_timeout 365 * 24 * 3600
     end
   end
 end
