40ecd4c6d0ee93707659759f3c3638808ac6bb78
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-08 14:48:01 +0530

    Added required `validations` and `associations` in `Customer`

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,40 @@
 # -*- warn_indent: true -*-
 
 class Customer < ApplicationRecord
-  include Filterable, Sortable
+  include Filterable, Sortable, ReferenceCode
 
   attribute :is_active, default: false
 
+  validates :reference_code,
+            length: {maximum: 15},
+            reduce: true
+  validates :name,
+            presence: true,
+            length: {maximum: 110},
+            reduce: true
+  validates :email,
+            uniqueness: true,
+            length: {maximum: 55},
+            email: true,
+            allow_blank: true,
+            allow_nil: true,
+            reduce: true
+  validates :mobile_number,
+            presence: true,
+            uniqueness: true,
+            length: {maximum: 32},
+            reduce: true
+
+  has_one :address, as: :addressable, dependent: :destroy
+
+  delegate :country, to: :address
+  delegate :name, to: :country, prefix: true
+
+  accepts_nested_attributes_for :address, update_only: true
+
   default_scope -> { order(arel_table[:reference_code].asc) }
+
+  def address
+    super.presence || build_address
+  end
 end
