62d440d0955d3871cbb65a935e79531402de2c65
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-08 14:48:21 +0530

    Added feature to `create` the `customer`

--- app/controllers/concerns/customers_shared.rb
+++ app/controllers/concerns/customers_shared.rb
@@ -8,11 +8,55 @@ module CustomersShared
   def self.included(base_class)
     base_class.class_eval do
 
-      # GET /(admin|manager|cashier)/suppliers
+      # GET /(admin|manager|cashier)/customers
       def index
         @customers = ::Customer.active.includes(:address)
         @pagy, @customers = pagy(@customers)
       end
+
+      # GET /(admin|manager|cashier)/customers/new
+      def new
+        @customer = ::Customer.new
+      end
+
+      # POST /(admin|manager|cashier)/customers
+      def create
+        response = ::Customers::CreateService.(customer_params)
+        @customer = response.payload[:customer]
+        if response.success?
+          flash[:notice] = response.message
+          redirect_to helpers.customers_path
+        else
+          flash.now[:alert] = response.message
+          respond_to do |format|
+            format.turbo_stream do
+              render turbo_stream: [
+                turbo_stream.update(:customer_form, partial: "customers/form"),
+                render_flash
+              ], status: :unprocessable_entity
+            end
+          end
+        end
+      end
     end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :name,
+      :email,
+      :mobile_number,
+      :is_active,
+      address_attributes: [
+        :address1,
+        :address2,
+        :city,
+        :region,
+        :country_id,
+        :postal_code
+      ]
+    )
   end
 end
