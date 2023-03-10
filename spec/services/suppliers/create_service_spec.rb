f4e84d5e83f45ae45dc538a7039c8cd489e58a7e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-10 11:37:33 +0530

    Added specs for `Suppliers::CreateService`

--- spec/services/suppliers/create_service_spec.rb
+++ spec/services/suppliers/create_service_spec.rb
@@ -7,4 +7,32 @@
 require "spec_helper"
 
 RSpec.describe Suppliers::CreateService, type: :service do
+  let(:supplier_attributes) { attributes_for(:supplier) }
+  subject { described_class.(supplier_attributes) }
+
+  describe "#call" do
+    context "when supplier is valid" do
+      it "returns a success response with the supplier" do
+        expect(subject).to be_a(ServiceResponse)
+        expect(subject).to be_success
+        expect(subject.message).to eq("Supplier '#{subject.payload[:supplier].name}' was successfully created.")
+        expect(subject.payload).to include(supplier: instance_of(::Supplier))
+      end
+
+      include_examples "creates a new object", ::Supplier
+    end
+
+    context "when supplier is invalid" do
+      let(:supplier_attributes) { attributes_for(:supplier, name: "") }
+
+      it "returns an error response with the supplier" do
+        expect(subject).to be_a(ServiceResponse)
+        expect(subject).to be_error
+        expect(subject.message).to eq("Supplier could not be created.")
+        expect(subject.payload).to include(supplier: instance_of(::Supplier))
+      end
+
+      include_examples "does not change count of objects", ::Supplier
+    end
+  end
 end
