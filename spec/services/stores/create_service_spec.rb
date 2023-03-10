5e6e5da110ac3b48209fd2b9e59c651ac18bf043
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-10 11:37:20 +0530

    Added specs for `Stores::CreateService`

--- spec/services/stores/create_service_spec.rb
+++ spec/services/stores/create_service_spec.rb
@@ -7,4 +7,32 @@
 require "spec_helper"
 
 RSpec.describe Stores::CreateService, type: :service do
+  let(:store_attributes) { attributes_for(:store) }
+  subject { described_class.(store_attributes) }
+
+  describe "#call" do
+    context "when store is valid" do
+      it "returns a success response with the store" do
+        expect(subject).to be_a(ServiceResponse)
+        expect(subject).to be_success
+        expect(subject.message).to eq("Store '#{subject.payload[:store].name}' was successfully created.")
+        expect(subject.payload).to include(store: instance_of(::Store))
+      end
+
+      include_examples "creates a new object", ::Store
+    end
+
+    context "when store is invalid" do
+      let(:store_attributes) { attributes_for(:store, name: "") }
+
+      it "returns an error response with the store" do
+        expect(subject).to be_a(ServiceResponse)
+        expect(subject).to be_error
+        expect(subject.message).to eq("Store could not be created.")
+        expect(subject.payload).to include(store: instance_of(::Store))
+      end
+
+      include_examples "does not change count of objects", ::Store
+    end
+  end
 end
