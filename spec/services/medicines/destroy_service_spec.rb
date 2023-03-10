a4ef2ae7e416a8daf1eac9c51ac66b37b917f929
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-10 12:10:11 +0530

    Added specs for `Medicines::DestroyService`

--- spec/services/medicines/destroy_service_spec.rb
+++ spec/services/medicines/destroy_service_spec.rb
@@ -7,4 +7,31 @@
 require "spec_helper"
 
 RSpec.describe Medicines::DestroyService, type: :service do
+  describe "#call" do
+    let(:medicine) { create(:medicine) }
+    subject { described_class.(medicine) }
+
+    context "when destroy is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Medicine '#{subject.payload[:medicine].reference_code} (#{medicine.name})' was successfully destroyed.")
+        expect(subject.payload[:medicine]).to eq(medicine)
+        expect(::Medicine.find_by(id: medicine.id)).to be_nil
+      end
+    end
+
+    context "when destroy fails" do
+      before do
+        allow(medicine).to receive(:destroy).and_return(false)
+      end
+
+      it "returns an error response" do
+        expect(subject).to be_error
+        expect(subject.message).to eq("Medicine '#{subject.payload[:medicine].reference_code} (#{medicine.name})' could not be destroyed.")
+        expect(subject.payload[:medicine]).to eq(medicine)
+      end
+
+      include_examples "does not change count of objects", ::Medicine
+    end
+  end
 end
