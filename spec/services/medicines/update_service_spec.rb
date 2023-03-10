ea105098f4e35872db4a0ec59beb832879c0a62d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-10 12:10:33 +0530

    Added specs for `Medicines::UpdateService`

--- spec/services/medicines/update_service_spec.rb
+++ spec/services/medicines/update_service_spec.rb
@@ -7,4 +7,30 @@
 require "spec_helper"
 
 RSpec.describe Medicines::UpdateService, type: :service do
+  describe "#call" do
+    let(:medicine) { create(:medicine) }
+    let(:medicine_attributes) { attributes_for(:medicine, name: "New name") }
+    subject { described_class.(medicine, medicine_attributes) }
+
+    context "when update is successful" do
+      it "returns a success response" do
+        expect(subject).to be_success
+        expect(subject.payload[:medicine]).to eq(medicine)
+        expect(medicine.reload.name).to eq(medicine.name)
+        expect(subject.message).to eq("Medicine '#{subject.payload[:medicine].reference_code} (#{medicine.name})' was successfully updated.")
+      end
+    end
+
+    context "when update fails" do
+      before do
+        allow(medicine).to receive(:update).and_return(false)
+      end
+
+      it "returns an error response" do
+        expect(subject).to be_error
+        expect(subject.message).to eq("Medicine could not be updated.")
+        expect(subject.payload[:medicine]).to eq(medicine)
+      end
+    end
+  end
 end
