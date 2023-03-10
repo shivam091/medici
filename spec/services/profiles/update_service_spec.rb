65945e093721a6a3ef542c4568a14e9400762941
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-10 12:49:18 +0530

    Added specs for `Profiles::UpdateService`

--- spec/services/profiles/update_service_spec.rb
+++ spec/services/profiles/update_service_spec.rb
@@ -7,4 +7,30 @@
 require "spec_helper"
 
 RSpec.describe Profiles::UpdateService, type: :service do
+  describe "#call" do
+    let(:user) { create(:cashier) }
+    let(:user_attributes) { attributes_for(:cashier, first_name: "New name") }
+    subject { described_class.(user, user_attributes) }
+
+    context "when update is successful" do
+      it "returns a success response" do
+        expect(subject).to be_success
+        expect(subject.payload[:user]).to eq(user)
+        expect(user.reload.full_name).to eq(user.full_name)
+        expect(subject.message).to eq("Your profile was successfully updated.")
+      end
+    end
+
+    context "when update fails" do
+      before do
+        allow(user).to receive(:update).and_return(false)
+      end
+
+      it "returns an error response" do
+        expect(subject).to be_error
+        expect(subject.message).to eq("Your profile could not be updated.")
+        expect(subject.payload[:user]).to eq(user)
+      end
+    end
+  end
 end
