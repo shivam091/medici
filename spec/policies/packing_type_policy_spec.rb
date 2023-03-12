9e0416b4387d8aa04ef7b355242f381f5daf155e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-12 12:22:27 +0530

    Added policy for packing types

---
+++ spec/policies/packing_type_policy_spec.rb
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/packing_type_policy_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PackingTypePolicy, type: :policy do
+
+end
