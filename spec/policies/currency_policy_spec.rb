f37c0046b58c94357a0c36b285467487d3057010
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-12 13:10:21 +0530

    Added policy for `Currency`

---
+++ spec/policies/currency_policy_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/currency_policy_spec.rb
+
+require "spec_helper"
+
+RSpec.describe CurrencyPolicy, type: :policy do
+end
