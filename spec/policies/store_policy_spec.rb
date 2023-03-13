67c2699badb0e4d7de8a87eee9f7f6361854f178
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-13 11:05:59 +0530

    Added policy for authorization of `stores` section

---
+++ spec/policies/store_policy_spec.rb
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/store_policy_spec.rb
+
+require "spec_helper"
+
+RSpec.describe StorePolicy, type: :policy do
+
+end
