156935496d0f6f828fe4479a6563fc7397666919
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-13 10:16:26 +0530

    Added policy for authorization of `users` section

---
+++ spec/policies/user_policy_spec.rb
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/user_policy_spec.rb
+
+require "spec_helper"
+
+RSpec.describe UserPolicy, type: :policy do
+
+end
