cfce7877f9ff037ba3d66488e7276e8e344f4cf1
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-13 09:45:03 +0530

    Added policy for authorization of `suppliers` section

---
+++ spec/policies/supplier_policy_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/supplier_policy_spec.rb
+
+require "spec_helper"
+
+RSpec.describe SupplierPolicy, type: :policy do
+end
