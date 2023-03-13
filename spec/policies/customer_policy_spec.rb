34ffbda967df0f194f69654af956834fc9cdd8f8
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-13 10:14:55 +0530

    Added policy for authorization of `customers` section

---
+++ spec/policies/customer_policy_spec.rb
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/customer_policy_spec.rb
+
+require "spec_helper"
+
+RSpec.describe CustomerPolicy, type: :policy do
+
+end
