54f0807712f5acffa93ba0ec43fda3ec61875181
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-13 10:42:31 +0530

    Added specs for `CustomerPolicy`

--- spec/policies/customer_policy_spec.rb
+++ spec/policies/customer_policy_spec.rb
@@ -7,5 +7,47 @@
 require "spec_helper"
 
 RSpec.describe CustomerPolicy, type: :policy do
+  let!(:customer) { create(:customer, :active) }
 
+  context "when admin is logged in" do
+    let(:admin) { build(:admin, :confirmed) }
+    subject { described_class.new(admin, customer) }
+
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:new) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
+
+    it { is_expected.to match_policy_scope(admin, [customer]) }
+  end
+
+  context "when manager is logged in" do
+    let(:manager) { build(:manager, :confirmed) }
+    subject { described_class.new(manager, customer) }
+
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:new) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to forbid_action(:destroy) }
+
+    it { is_expected.to match_policy_scope(manager, [customer]) }
+  end
+
+  context "when cashier is logged in" do
+    let(:cashier) { build(:cashier, :confirmed) }
+    subject { described_class.new(cashier, customer) }
+
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:new) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to forbid_action(:destroy) }
+
+    it { is_expected.to match_policy_scope(cashier, [customer]) }
+  end
 end
