38dba56f7c8991755ec1fa03f49d6c9ffbcfb816
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-17 19:31:17 +0530

    Added feature to `create` the `shift`

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -9,4 +9,35 @@ class Admin::ShiftsController < Admin::BaseController
     @shifts = ::Shift.active
     @pagy, @shifts = pagy(@shifts)
   end
+
+  # GET /admin/shifts/new
+  def new
+    @shift = ::Shift.new
+  end
+
+  # POST /admin/shifts
+  def create
+    response = ::Shifts::CreateService.(shift_params)
+    @shift = response.payload[:shift]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_shifts_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:shift_form, partial: "admin/shifts/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:name, :starts_at, :ends_at, :is_active)
+  end
 end
