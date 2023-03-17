fb945a632b647bf34e3a121f05f5420634c58d26
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-17 19:12:25 +0530

    Added `default scope` and `validations` in `Shift`

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,4 +3,17 @@
 # -*- warn_indent: true -*-
 
 class Shift < ApplicationRecord
+  include Filterable, Sortable
+
+  attribute :is_active, default: false
+
+  validates :name,
+            presence: true,
+            length: {maximum: 55},
+            reduce: true
+  validates :starts_at, :ends_at,
+            presence: true,
+            reduce: true
+
+  default_scope -> { order_name_asc }
 end
