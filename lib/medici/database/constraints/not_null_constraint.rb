bbde24df4d8d91c8f8901a96ea3431ce9457db2e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-18 17:25:06 +0530

    Moved `add_not_null_constraint` method to separate module

---
+++ lib/medici/database/constraints/not_null_constraint.rb
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+module Medici
+  module Database
+    module Constraints
+      module NotNullConstraint
+        # Returns the name for a not null constraint
+        def not_null_constraint_name(table, column, name: nil)
+          name.presence || check_constraint_name(table, column, "not_null")
+        end
+
+        # Returns definitions for a not null constraint
+        def not_null_constraint_definitions(column_name, options)
+          definitions = "#{quote_column_name(column_name)} IS NOT NULL"
+          conditions_with_if(definitions, options)
+        end
+
+        # Helper for adding not null constraint to the column.
+        def add_not_null_constraint(table, column_name, options = {})
+          add_check_constraint(
+            table,
+            not_null_constraint_definitions(column_name),
+            name: not_null_constraint_name(table, column_name, name: options.delete(:name))
+          )
+        end
+      end
+    end
+  end
+end
