a2f5c6f16356cbe63d4a8e0a592168a3e31761fe
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-19 19:40:12 +0530

    Updated policies to forbid `destroy` to `admin` and added context for `super admin`

--- spec/policies/currency_policy_spec.rb
+++ spec/policies/currency_policy_spec.rb
@@ -9,6 +9,20 @@ require "spec_helper"
 RSpec.describe CurrencyPolicy, type: :policy do
   let!(:currency) { create(:currency, :active) }
 
+  context "when super admin is logged in" do
+    let(:super_admin) { build(:super_admin, :confirmed) }
+    subject { described_class.new(super_admin, currency) }
+
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:new) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
+
+    it { is_expected.to match_policy_scope(super_admin, [currency]) }
+  end
+
   context "when admin is logged in" do
     let(:admin) { build(:admin, :confirmed) }
     subject { described_class.new(admin, currency) }
@@ -18,7 +32,7 @@ RSpec.describe CurrencyPolicy, type: :policy do
     it { is_expected.to permit_action(:create) }
     it { is_expected.to permit_action(:edit) }
     it { is_expected.to permit_action(:update) }
-    it { is_expected.to permit_action(:destroy) }
+    it { is_expected.to forbid_action(:destroy) }
 
     it { is_expected.to match_policy_scope(admin, [currency]) }
   end
