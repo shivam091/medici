a6e8f9d03b4d37c338634cb872338cda199f02b8
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 19:01:14 +0530

    Added policy for `Expense`

---
+++ app/policies/expense_policy.rb
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class ExpensePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if (user.super_admin? || user.admin?)
+        scope.all
+      elsif (user.manager? || user.cashier?)
+        user.expenses
+      else
+        scope.none
+      end
+    end
+  end
+
+  def index?
+    (user.super_admin? || user.admin? || user.manager? || user.cashier?)
+  end
+
+  def inactive?
+    (user.super_admin? || user.admin? || user.manager? || user.cashier?)
+  end
+
+  def new?
+    (user.super_admin? || user.admin? || user.manager? || user.cashier?)
+  end
+
+  def create?
+    (user.super_admin? || user.admin? || user.manager? || user.cashier?)
+  end
+
+  def edit?
+    (user.super_admin? || user.admin? || user.manager? || user.cashier?)
+  end
+
+  def update?
+    (user.super_admin? || user.admin? || user.manager? || user.cashier?)
+  end
+
+  def activate?
+    (user.super_admin? || user.admin?)
+  end
+
+  def deactivate?
+    (user.super_admin? || user.admin?)
+  end
+
+  def destroy?
+    user.super_admin?
+  end
+end
