4eaa057929c14666248dc11dbc9abf110e40aa0a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 17:39:28 +0530

    Added required `attributes` and `associations` for `Expense` model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,12 @@
 # -*- warn_indent: true -*-
 
 class Expense < ApplicationRecord
+  include Sortable
+
+  attribute :amount, default: 0.0
+
+  belongs_to :store, inverse_of: :expenses
+  belongs_to :user, inverse_of: :expenses
+
+  default_scope -> { order_created_desc }
 end
--- db/migrate/20230325115750_create_expenses.rb
+++ db/migrate/20230325115750_create_expenses.rb
@@ -5,6 +5,32 @@
 class CreateExpenses < Medici::Database::Migration[1.0]
   def change
     create_table_with_constraints :expenses, id: :uuid do |t|
+      t.references :store,
+                   type: :uuid,
+                   foreign_key: {
+                     to_table: :stores,
+                     name: :fk_expenses_store_id_on_stores,
+                     on_delete: :cascade
+                   },
+                   index: {using: :btree}
+      t.references :user,
+                   type: :uuid,
+                   foreign_key: {
+                     to_table: :users,
+                     name: :fk_expenses_user_id_on_users,
+                     on_delete: :nullify
+                   },
+                   index: {using: :btree}
+      t.string :criteria
+      t.decimal :amount, precision: 8, scale: 2, default: 0.0
+
+      t.not_null_and_empty_constraint :criteria
+
+      t.not_null_constraint :amount
+      t.not_null_constraint :store_id
+      t.not_null_constraint :user_id
+
+      t.numericality_constraint :amount, greater_than: 0.0
 
       t.timestamps_with_timezone null: false
     end
