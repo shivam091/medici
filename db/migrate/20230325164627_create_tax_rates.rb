676d2896bb5066cb1dd4ce4c81cd9eb09898ca2c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 22:19:34 +0530

    Added model `TaxRate`

---
+++ db/migrate/20230325164627_create_tax_rates.rb
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class CreateTaxRates < Medici::Database::Migration[1.0]
+  def change
+    create_table_with_constraints :tax_rates, id: :uuid do |t|
+
+      t.timestamps_with_timezone null: false
+    end
+  end
+end
