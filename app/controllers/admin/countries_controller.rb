cd6f8a1c39130fb820a4ec3c9fb6caa8dfbc501a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 12:14:34 +0530

    Added feature to `activate` the `country`

--- app/controllers/admin/countries_controller.rb
+++ app/controllers/admin/countries_controller.rb
@@ -86,6 +86,18 @@ class Admin::CountriesController < Admin::BaseController
     redirect_to admin_countries_path
   end
 
+  # PATCH /admin/countries/:uuid/activate
+  def activate
+    response = ::Countries::ActivateService.(@country)
+    @country = response.payload[:country]
+    if response.success?
+      flash[:notice] = response.message
+    else
+      flash[:alert] = response.message
+    end
+    redirect_to inactive_admin_currencies_path
+  end
+
   private
 
   def country_params
