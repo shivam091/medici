004112cc464f6e2993e005710d802080382142dc
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 14:55:32 +0530

    Added service to `deactivate` the `shift`

---
+++ spec/services/shifts/deactivate_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/shifts/deactivate_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Shifts::DeactivateService, type: :service do
+end
