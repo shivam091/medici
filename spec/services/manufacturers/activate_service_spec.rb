e90c3e4391804d272d043cc0c52bf63b60348611
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 11:08:03 +0530

    Added service to `activate` the `manufacturer`

---
+++ spec/services/manufacturers/activate_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/manufacturers/activate_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Manufacturers::ActivateService, type: :service do
+end
