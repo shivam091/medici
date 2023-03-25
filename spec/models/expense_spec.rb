36bbd632b4b272cc43551a63f7d74db75f057fdf
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 17:28:42 +0530

    Added model `Expense`

---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/models/expense_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Expense, type: :model do
+end
