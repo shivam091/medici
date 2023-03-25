a3e39fa7a9ab857e09217f2bd672e89bacc50ba6
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 19:01:03 +0530

    Added reference code for `expenses`

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,7 +3,7 @@
 # -*- warn_indent: true -*-
 
 class Expense < ApplicationRecord
-  include Sortable
+  include Sortable, ReferenceCode
 
   attribute :amount, default: 0.0
 
@@ -11,6 +11,8 @@ class Expense < ApplicationRecord
   belongs_to :user, inverse_of: :expenses
 
   after_initialize :set_store
+
+  delegate :name, :phone_number, :email, to: :store, prefix: true
 
   default_scope -> { order_created_desc }
 
