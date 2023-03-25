a17d31501767906dc7ab757c33678cbbda4e841f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 14:19:20 +0530

    Added service to `activate` the `packing type`

---
+++ spec/services/packing_types/activate_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/packing_types/activate_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PackingTypes::ActivateService, type: :service do
+end
