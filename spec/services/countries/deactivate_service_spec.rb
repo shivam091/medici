0568cc8bd5744ebe9f2ce5f9d870d9e3a4b4f0eb
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-25 12:14:57 +0530

    Added service to `deactivate` the `country`

---
+++ spec/services/countries/deactivate_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/countries/deactivate_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Countries::DeactivateService, type: :service do
+end
