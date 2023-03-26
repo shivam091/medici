0d05684d0e7f296f0778f07953d2131a2e6758f2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-26 22:04:44 +0530

    Added validations in `PurchaseOrder` and `PurchaseOrderMedicine`, required configurations, and state machine

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -3,7 +3,57 @@
 # -*- warn_indent: true -*-
 
 class PurchaseOrder < ApplicationRecord
-  include ReferenceCode
+  include Sortable, ReferenceCode, AASM
+
+  enum status: {
+    pending: "pending",
+    incomplete: "incomplete",
+    received: "received"
+  }
+
+  attribute :status, :enum, default: statuses[:pending]
+
+  aasm :purchase_order_status, column: "status", enum: true do
+    state :pending, initial: true
+    state :incomplete, :received
+
+    event :mark_as_incomplete do
+      transitions from: :pending, to: :incomplete
+    end
+
+    event :mark_as_received do
+      transitions from: [:pending, :incomplete], to: :received
+    end
+  end
+
+  validates :reference_code,
+            uniqueness: true,
+            length: {maximum: 15},
+            reduce: true
+  validates :invoice_number,
+            presence: true,
+            uniqueness: true,
+            length: {maximum: 55},
+            reduce: true
+  validates :supplier_id,
+            presence: true,
+            reduce: true
+  validates :tracking_number,
+            length: {maximum: 55},
+            allow_blank: true,
+            allow_nil: true
+  validates :status,
+            presence: true,
+            inclusion: {in: statuses.values},
+            reduce: true
+  validates :ordered_at,
+            presence: true,
+            comparison: {less_than_or_equal_to: Date.today},
+            reduce: true
+  validates :expected_arrival_at,
+            comparison: {greater_than_or_equal_to: Date.today},
+            allow_nil: true,
+            reduce: true
 
   has_many :purchase_order_medicines, dependent: :destroy
   has_many :medicines,
@@ -14,4 +64,20 @@ class PurchaseOrder < ApplicationRecord
   belongs_to :store, inverse_of: :purchase_orders
   belongs_to :supplier, inverse_of: :purchase_orders
   belongs_to :user, inverse_of: :purchase_orders
+
+  accepts_nested_attributes_for :purchase_order_medicines,
+                                allow_destroy: true,
+                                reject_if: :reject_purchase_order_medicine?
+
+  default_scope -> { order_reference_code_asc }
+
+  private
+
+  def reject_purchase_order_medicine?(attributes)
+    [
+      attributes[:medicine_id],
+      attributes[:quantity],
+      attributes[:cost]
+    ].all?(&:blank?)
+  end
 end
--- app/models/purchase_order_medicine.rb
+++ app/models/purchase_order_medicine.rb
@@ -3,6 +3,21 @@
 # -*- warn_indent: true -*-
 
 class PurchaseOrderMedicine < ApplicationRecord
+  attribute :quantity, default: 1
+  attribute :cost, default: 0.0
+
+  validates :medicine_id,
+            presence: true,
+            reduce: true
+  validates :quantity,
+            presence: true,
+            numericality: {greater_than: 0},
+            reduce: true
+  validates :cost,
+            presence: true,
+            numericality: {greater_than: 0.0},
+            reduce: true
+
   belongs_to :purchase_order, inverse_of: :purchase_order_medicines, touch: true
   belongs_to :medicine, inverse_of: :purchase_order_medicines
 end
