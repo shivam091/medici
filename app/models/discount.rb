98cb122cf7b3c8c70559e80c35c63ff2dc2a4909
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-26 10:38:49 +0530

    Added `default scope` and `validations` in `Discount`

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,5 +3,26 @@
 # -*- warn_indent: true -*-
 
 class Discount < ApplicationRecord
-  belongs_to :country, inverse_of: :discount
+  validates :country_id,
+            presence: true,
+            uniqueness: {message: :already_has_discount},
+            reduce: true
+  validates :percent_off,
+            presence: true,
+            numericality: {greater_than_or_equal_to: 0.0},
+            reduce: true
+
+  belongs_to :country, inverse_of: :tax_rate
+
+  delegate :name, :iso2, :iso3, to: :country, prefix: true
+
+  default_scope do
+    discounts_arel = ::Discount.arel_table
+    countries_arel = ::Country.arel_table
+
+    join = discounts_arel.join(countries_arel)
+      .on(discounts_arel[:country_id].eq(countries_arel[:id]))
+      .join_sources
+    joins(join).order(countries_arel[:iso2].asc)
+  end
 end
