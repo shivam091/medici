c902006fd568fba1e5a3edaf5daa6a8e06220ca0
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-26 10:56:10 +0530

    Added feature to `create` the `discount`

--- app/controllers/admin/discounts_controller.rb
+++ app/controllers/admin/discounts_controller.rb
@@ -16,4 +16,35 @@ class Admin::DiscountsController < Admin::BaseController
     @discounts = policy_scope(::Discount).includes(:country)
     @pagy, @discounts = pagy(@discounts)
   end
+
+  # GET /admin/discounts/new
+  def new
+    @discount = ::Discount.new
+  end
+
+  # POST /admin/discounts
+  def create
+    response = ::Discounts::CreateService.(discount_params)
+    @discount = response.payload[:discount]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_discounts_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:discount_form, partial: "admin/discounts/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:country_id, :percent_off)
+  end
 end
