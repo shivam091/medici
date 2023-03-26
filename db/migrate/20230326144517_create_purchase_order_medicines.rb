62802a5d0bc7cc0ebd6c05154168194c85d22c3a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-26 20:16:20 +0530

    Added model `PurchaseOrderMedicine`

---
+++ db/migrate/20230326144517_create_purchase_order_medicines.rb
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class CreatePurchaseOrderMedicines < Medici::Database::Migration[1.0]
+  def change
+    create_table_with_constraints :purchase_order_medicines, id: :uuid do |t|
+
+      t.timestamps_with_timezone null: false
+    end
+  end
+end
