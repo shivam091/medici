d4a11820675375b77e87c97246f254666feae1cf
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-27 18:00:50 +0530

    Added feature to `create` the `purchase order`

--- app/controllers/concerns/purchase_orders_shared.rb
+++ app/controllers/concerns/purchase_orders_shared.rb
@@ -20,6 +20,51 @@ module PurchaseOrdersShared
         @purchase_orders = policy_scope(::PurchaseOrder).includes(:purchase_order_medicines)
         @pagy, @purchase_orders = pagy(@purchase_orders)
       end
+
+      # GET /(admin|manager)/purchase-orders/new
+      def new
+        @purchase_order = current_user.purchase_orders.build
+      end
+
+      # POST /(admin|manager)/purchase-orders
+      def create
+        response = ::PurchaseOrders::CreateService.(current_user, purchase_order_params)
+        @purchase_order = response.payload[:purchase_order]
+        if response.success?
+          flash[:notice] = response.message
+          redirect_to helpers.purchase_orders_path
+        else
+          flash.now[:alert] = response.message
+          respond_to do |format|
+            format.turbo_stream do
+              render turbo_stream: [
+                turbo_stream.update(:purchase_order_form, partial: "purchase_orders/form"),
+                render_flash
+              ], status: :unprocessable_entity
+            end
+          end
+        end
+      end
     end
+  end
+
+  private
+
+  def purchase_order_params
+    params.require(:purchase_order).permit(
+      :supplier_id,
+      :invoice_number,
+      :tracking_number,
+      :ordered_at,
+      :expected_arrival_at,
+      purchase_order_medicines_attributes: [
+        :id,
+        :_destroy,
+        :medicine_id,
+        :quantity,
+        :cost,
+        :is_received
+      ]
+    )
   end
 end
