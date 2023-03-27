4e4f147ecbf23fde77ae99bf0c721e424fa38af4
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-27 09:43:38 +0530

    Added policy for authorization of purchase orders section

---
+++ app/policies/purchase_order_policy.rb
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class PurchaseOrderPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if (user.super_admin? || user.admin?)
+        scope.all
+      elsif user.manager?
+        user.purchase_orders
+      else
+        scope.none
+      end
+    end
+  end
+
+  def index?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def pending?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def incomplete?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def completed?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def new?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def create?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def edit?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def update?
+    (user.super_admin? || user.admin? || user.manager?)
+  end
+
+  def destroy?
+    user.super_admin?
+  end
+end
---
+++ spec/policies/purchase_order_policy_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/policies/purchase_order_policy_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PurchaseOrderPolicy, type: :policy do
+end
