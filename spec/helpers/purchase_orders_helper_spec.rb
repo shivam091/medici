50fefb21dec83fb19c8846639d0a8f1c724623fc
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-27 09:43:22 +0530

    Added helpers to resolve routes of `purchase orders` section

---
+++ spec/helpers/purchase_orders_helper_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/helpers/purchase_orders_helper_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PurchaseOrdersHelper, type: :helper do
+end
