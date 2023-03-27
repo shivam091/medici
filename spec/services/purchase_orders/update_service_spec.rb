9e8a7d5fc354ad9f392f5020dba2ce2652fab004
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-27 18:52:11 +0530

    Added service to `update` the `purchase order`

---
+++ spec/services/purchase_orders/update_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/purchase_orders/update_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PurchaseOrders::UpdateService, type: :service do
+end
