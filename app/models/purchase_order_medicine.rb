d9ec8e505d198e7760bbbb90c562ca50859e35dc
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-28 22:40:06 +0530

    Added callbacks to `update replenishment` when `purchase order` is `created`

--- app/models/purchase_order_medicine.rb
+++ app/models/purchase_order_medicine.rb
@@ -21,5 +21,42 @@ class PurchaseOrderMedicine < ApplicationRecord
   belongs_to :purchase_order, inverse_of: :purchase_order_medicines, touch: true
   belongs_to :medicine, inverse_of: :purchase_order_medicines
 
+  before_create :add_quantity_to_replenishment
+  before_update :update_quantity_of_replenishment
+  before_destroy :subtract_quantity_from_replenishment
+
   delegate :name, to: :medicine, prefix: true
+
+  private
+
+  def add_quantity_to_replenishment
+    replenishment = self.medicine.replenishment
+    replenishment.lock!
+
+    replenishment.update_column(
+      :quantity_pending_from_supplier,
+      (replenishment.quantity_pending_from_supplier + self.quantity)
+    )
+  end
+
+  def subtract_quantity_from_replenishment
+    replenishment = self.medicine.replenishment
+    replenishment.lock!
+
+    replenishment.update_column(
+      :quantity_pending_from_supplier,
+      (replenishment.quantity_pending_from_supplier - self.quantity)
+    )
+  end
+
+  def update_quantity_of_replenishment
+    replenishment = self.medicine.replenishment
+    replenishment.lock!
+
+    difference_in_quantity = quantity - quantity_was
+    replenishment.update_column(
+      :quantity_pending_from_supplier,
+      (replenishment.quantity_pending_from_supplier + difference_in_quantity)
+    )
+  end
 end
