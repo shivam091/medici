866a0216bceaa83dfe5a21c665ecf7ba936f8a00
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-29 15:59:36 +0530

    Conditional no object text for `ingredients`

--- config/locales/en/admin/ingredients.rb
+++ config/locales/en/admin/ingredients.rb
@@ -25,7 +25,9 @@
           delete_ingredient_confirmation_text: "Are you sure you want to delete the ingredient '%{ingredient_name}'?",
           activate_ingredient_confirmation_text: "Are you sure you want to activate the ingredient '%{ingredient_name}'?",
           deactivate_ingredient_confirmation_text: "Are you sure you want to deactivate the ingredient '%{ingredient_name}'?",
-          no_ingredients_to_display: "No ingredients to display."
+          no_ingredients_to_display: "No ingredients to display.",
+          no_active_ingredients_to_display: "There are no active ingredients to display.",
+          no_inactive_ingredients_to_display: "There are no inactive ingredients to display.",
         },
         new: {
           title: "New ingredient",
