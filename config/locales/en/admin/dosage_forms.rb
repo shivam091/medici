79e47abee2869fa81eece9c5b302f4b085e2ab79
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-29 15:41:14 +0530

    Conditional no object text for `dosage forms`

--- config/locales/en/admin/dosage_forms.rb
+++ config/locales/en/admin/dosage_forms.rb
@@ -24,7 +24,9 @@
           delete_dosage_form_confirmation_text: "Are you sure you want to delete the dosage form '%{dosage_form_name}'?",
           activate_dosage_form_confirmation_text: "Are you sure you want to activate the dosage form '%{dosage_form_name}'?",
           deactivate_dosage_form_confirmation_text: "Are you sure you want to deactivate the dosage form '%{dosage_form_name}'?",
-          no_dosage_forms_to_display: "No dosage forms to display."
+          no_dosage_forms_to_display: "There are no dosage forms to display.",
+          no_active_dosage_forms_to_display: "There are no active dosage forms to display.",
+          no_inactive_dosage_forms_to_display: "There are no inactive dosage forms to display.",
         },
         new: {
           title: "New dosage form",
