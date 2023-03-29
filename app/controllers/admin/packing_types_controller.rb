4de218973a65ad62ec771d2368af9f0f8dc93e20
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-29 11:38:12 +0530

    Added active section in `packing types`

--- app/controllers/admin/packing_types_controller.rb
+++ app/controllers/admin/packing_types_controller.rb
@@ -4,9 +4,9 @@
 
 class Admin::PackingTypesController < Admin::BaseController
 
-  before_action :find_packing_type, except: [:index, :inactive, :new, :create]
+  before_action :find_packing_type, except: [:index, :active, :inactive, :new, :create]
   before_action do
-    if action_name.in?(["index", "inactive", "new", "create"])
+    if action_name.in?(["index", "active", "inactive", "new", "create"])
       authorize ::PackingType
     else
       authorize @packing_type
@@ -15,6 +15,12 @@ class Admin::PackingTypesController < Admin::BaseController
 
   # GET /admin/packing-types
   def index
+    @packing_types = policy_scope(::PackingType).active
+    @pagy, @packing_types = pagy(@packing_types)
+  end
+
+  # GET /admin/packing-types/active
+  def active
     @packing_types = policy_scope(::PackingType).active
     @pagy, @packing_types = pagy(@packing_types)
   end
