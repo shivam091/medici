f5629f64070783d9662d60732b8bbf3f3139ff34
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-30 21:36:12 +0530

    Tile on `admin` section for `expenses` and `broadcasting` count

--- config/locales/en/manager/dashboard.rb
+++ config/locales/en/manager/dashboard.rb
@@ -16,6 +16,7 @@
           total_purchase_orders: "Total purchase orders",
           low_stock_medicines: "Low stock medicines",
           out_of_stock_medicines: "Out of stock medicines",
+          total_expenses: "Total expenses",
           nearby_expiry: "Nearby expiry",
           expired_medicines: "Expired medicines",
           view_all: "View all",
