50f1b1e5e8de95e846ede98b5199dc0c80158d1f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-03-31 10:01:29 +0530

    Added scope `with_address` in `Supplier`

--- app/controllers/concerns/suppliers_shared.rb
+++ app/controllers/concerns/suppliers_shared.rb
@@ -19,19 +19,19 @@ module SuppliersShared
 
       # GET /(admin|manager)/suppliers
       def index
-        @suppliers = policy_scope(::Supplier).includes(:address)
+        @suppliers = policy_scope(::Supplier).including_address
         @pagy, @suppliers = pagy(@suppliers)
       end
 
       # GET /(admin|manager)/suppliers/active
       def active
-        @suppliers = policy_scope(::Supplier).active.includes(:address)
+        @suppliers = policy_scope(::Supplier).active.including_address
         @pagy, @suppliers = pagy(@suppliers)
       end
 
       # GET /(admin|manager)/suppliers/inactive
       def inactive
-        @suppliers = policy_scope(::Supplier).inactive.includes(:address)
+        @suppliers = policy_scope(::Supplier).inactive.including_address
         @pagy, @suppliers = pagy(@suppliers)
       end
 
