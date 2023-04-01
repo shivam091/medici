2e5397abbbb9fe5990e59bb6bf664dfa9a2f6bbf
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-01 09:51:23 +0530

    Added callback `#update_po_status` to update purchase order status

--- app/models/purchase_order_medicine.rb
+++ app/models/purchase_order_medicine.rb
@@ -21,5 +21,21 @@ class PurchaseOrderMedicine < ApplicationRecord
   belongs_to :purchase_order, inverse_of: :purchase_order_medicines, touch: true
   belongs_to :medicine, inverse_of: :purchase_order_medicines
 
+  after_save :update_po_status, if: :is_received_previously_changed?
+
   delegate :name, to: :medicine, prefix: true
+
+  scope :received, -> { where(::PurchaseOrderMedicine[:is_received].eq(true)) }
+
+  private
+
+  def update_po_status
+    po_medicines = purchase_order.purchase_order_medicines
+
+    if po_medicines.received.size == po_medicines.size
+      purchase_order.mark_as_received!
+    elsif is_received? && !purchase_order.incomplete?
+      purchase_order.mark_as_incomplete!
+    end
+  end
 end
