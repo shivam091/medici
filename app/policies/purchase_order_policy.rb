30e9e2db79d6c475c854768abfeee49b56360b76
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-02 19:30:14 +0530

    Changes in `PurchaseOrderPolicy`

--- app/policies/purchase_order_policy.rb
+++ app/policies/purchase_order_policy.rb
@@ -26,22 +26,22 @@ class PurchaseOrderPolicy < ApplicationPolicy
   end
 
   def new?
-    (user.super_admin? || user.admin? || user.manager?)
+    (user.super_admin? || user.admin?)
   end
 
   def create?
-    (user.super_admin? || user.admin? || user.manager?)
+    (user.super_admin? || user.admin?)
   end
 
   def edit?
-    (user.super_admin? || user.admin? || user.manager?)
+    (user.super_admin? || user.admin?) && !record.received?
   end
 
   def update?
-    (user.super_admin? || user.admin? || user.manager?)
+    (user.super_admin? || user.admin?) && !record.received?
   end
 
   def destroy?
-    user.super_admin? && record.pending?
+    (user.super_admin? || user.admin?) && record.pending?
   end
 end
