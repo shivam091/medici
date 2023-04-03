38659451774c9468093a776f40ae395329a79ecf
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-03 10:25:52 +0530

    Moved actions from `PurchaseOrdersShared` to `PurchaseOrdersController`

--- app/controllers/admin/purchase_orders_controller.rb
+++ app/controllers/admin/purchase_orders_controller.rb
@@ -3,5 +3,4 @@
 # -*- warn_indent: true -*-
 
 class Admin::PurchaseOrdersController < PurchaseOrdersController
-  include PurchaseOrdersShared
 end
--- app/controllers/manager/purchase_orders_controller.rb
+++ app/controllers/manager/purchase_orders_controller.rb
@@ -3,5 +3,4 @@
 # -*- warn_indent: true -*-
 
 class Manager::PurchaseOrdersController < PurchaseOrdersController
-  include PurchaseOrdersShared
 end
