2d687d07243c5d692c547c592864b1a222480cde
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-03 10:34:23 +0530

    Shown address in profile

--- config/locales/en/profiles.rb
+++ config/locales/en/profiles.rb
@@ -8,11 +8,12 @@
       show: {
         title: "Your profile",
         your_profile: "Your profile",
-        edit_profile_details: "Edit your profile details viz. first name, last name, etc.",
+        edit_profile_details: "Edit your profile details viz. first name, last name, address, etc.",
         name: "Name",
         mobile_number: "Mobile number",
         email: "Email address",
         password: "Password",
+        address: "Address",
         mobile_number_help_text: "Quickly sign in, easily recover passwords, and receive security notifications with this mobile number.",
         login_and_security_preferences: "Login & security preferences",
         login_and_security_preferences_help_text: "Edit mobile number or login settings",
