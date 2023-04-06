76a18cfb69a15b2093a4bd88bf751f6f862c6cb5
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 12:36:26 +0530

    Added specs for `Currencies::Activate` and `Currencies::Deactivate` services

--- spec/services/currencies/activate_service_spec.rb
+++ spec/services/currencies/activate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe Currencies::ActivateService, type: :service do
+  describe "#call" do
+    let(:currency) { create(:currency) }
+    subject { described_class.(currency) }
+
+    context "when activation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Currency '#{currency.name}' was successfully activated.")
+        expect(subject.payload[:currency]).to eq(currency)
+      end
+    end
+
+    context "when activation fails" do
+      it "returns an error response" do
+        allow(currency).to receive(:activate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Currency '#{currency.name}' could not be activated.")
+        expect(subject.payload[:currency]).to eq(currency)
+      end
+    end
+  end
 end
--- spec/services/currencies/deactivate_service_spec.rb
+++ spec/services/currencies/deactivate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe Currencies::DeactivateService, type: :service do
+  describe "#call" do
+    let(:currency) { create(:currency) }
+    subject { described_class.(currency) }
+
+    context "when deactivation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Currency '#{currency.name}' was successfully deactivated.")
+        expect(subject.payload[:currency]).to eq(currency)
+      end
+    end
+
+    context "when deactivation fails" do
+      it "returns an error response" do
+        allow(currency).to receive(:deactivate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Currency '#{currency.name}' could not be deactivated.")
+        expect(subject.payload[:currency]).to eq(currency)
+      end
+    end
+  end
 end
