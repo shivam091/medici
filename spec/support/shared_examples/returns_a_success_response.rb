353c6d6a7998ecfb7b98f16175ed3a0a9a5b158e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 19:57:21 +0530

    Added `shared examples` for checking service response status

---
+++ spec/support/shared_examples/returns_a_success_response.rb
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+RSpec.shared_examples "returns a success response" do |klass|
+  it "returns a success response" do
+    expect(subject).to be_success
+  end
+end
---
+++ spec/support/shared_examples/returns_an_error_response.rb
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+RSpec.shared_examples "returns an error response" do |klass|
+  it "returns an error response" do
+    expect(subject).to be_error
+  end
+end
