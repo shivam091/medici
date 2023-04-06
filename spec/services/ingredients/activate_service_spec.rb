33a13fe3350f27639f44bde8b6d704947c18c5b2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 12:52:38 +0530

    Added specs for `Ingredients::Activate` and `Ingredients::Deactivate` services

--- spec/services/ingredients/activate_service_spec.rb
+++ spec/services/ingredients/activate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe Ingredients::ActivateService, type: :service do
+  describe "#call" do
+    let(:ingredient) { create(:ingredient) }
+    subject { described_class.(ingredient) }
+
+    context "when activation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Ingredient '#{ingredient.name}' was successfully activated.")
+        expect(subject.payload[:ingredient]).to eq(ingredient)
+      end
+    end
+
+    context "when activation fails" do
+      it "returns an error response" do
+        allow(ingredient).to receive(:activate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Ingredient '#{ingredient.name}' could not be activated.")
+        expect(subject.payload[:ingredient]).to eq(ingredient)
+      end
+    end
+  end
 end
--- spec/services/ingredients/deactivate_service_spec.rb
+++ spec/services/ingredients/deactivate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe Ingredients::DeactivateService, type: :service do
+  describe "#call" do
+    let(:ingredient) { create(:ingredient) }
+    subject { described_class.(ingredient) }
+
+    context "when deactivation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Ingredient '#{ingredient.name}' was successfully deactivated.")
+        expect(subject.payload[:ingredient]).to eq(ingredient)
+      end
+    end
+
+    context "when deactivation fails" do
+      it "returns an error response" do
+        allow(ingredient).to receive(:deactivate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Ingredient '#{ingredient.name}' could not be deactivated.")
+        expect(subject.payload[:ingredient]).to eq(ingredient)
+      end
+    end
+  end
 end
