4f840f0fdaac3fe0d57f08fab871562293984dc6
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 14:51:26 +0530

    Added specs for `Countries::Activate` and `Countries::Deactivate` services

--- spec/services/countries/activate_service_spec.rb
+++ spec/services/countries/activate_service_spec.rb
@@ -7,4 +7,34 @@
 require "spec_helper"
 
 RSpec.describe Countries::ActivateService, type: :service do
+  describe "#call" do
+    let(:country) { create(:country) }
+    subject { described_class.(country) }
+
+    context "when activation is successful" do
+      it "activates the country" do
+        expect { subject }.to change { country.reload.is_active? }.to(true)
+      end
+
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Country '#{country.name}' was successfully activated.")
+      end
+    end
+
+    context "when activation fails" do
+      before do
+        allow(country).to receive(:activate!).and_return(false)
+      end
+
+      it "does not activate the country" do
+        expect { subject }.not_to change { country.reload.is_active? }
+      end
+
+      it "returns an error response" do
+        expect(subject).to be_error
+        expect(subject.message).to eq("Country '#{country.name}' could not be activated.")
+      end
+    end
+  end
 end
--- spec/services/countries/deactivate_service_spec.rb
+++ spec/services/countries/deactivate_service_spec.rb
@@ -7,4 +7,34 @@
 require "spec_helper"
 
 RSpec.describe Countries::DeactivateService, type: :service do
+  describe "#call" do
+    let(:country) { create(:country, :active) }
+    subject { described_class.(country) }
+
+    context "when deactivation is successful" do
+      it "deactivates the country" do
+        expect { subject }.to change { country.reload.is_active? }.to(false)
+      end
+
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Country '#{country.name}' was successfully deactivated.")
+      end
+    end
+
+    context "when deactivation fails" do
+      before do
+        allow(country).to receive(:deactivate!).and_return(false)
+      end
+
+      it "does not deactivate the country" do
+        expect { subject }.not_to change { country.reload.is_active? }
+      end
+
+      it "returns an error response" do
+        expect(subject).to be_error
+        expect(subject.message).to eq("Country '#{country.name}' could not be deactivated.")
+      end
+    end
+  end
 end
