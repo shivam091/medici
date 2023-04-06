e0ec8809ae54caf25c2c439c51744cabe7ba95ee
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 12:52:58 +0530

    Added specs for `PackingTypes::Activate` and `PackingTypes::Deactivate` services

--- spec/services/packing_types/activate_service_spec.rb
+++ spec/services/packing_types/activate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe PackingTypes::ActivateService, type: :service do
+  describe "#call" do
+    let(:packing_type) { create(:packing_type) }
+    subject { described_class.(packing_type) }
+
+    context "when activation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Packing type '#{packing_type.name}' was successfully activated.")
+        expect(subject.payload[:packing_type]).to eq(packing_type)
+      end
+    end
+
+    context "when activation fails" do
+      it "returns an error response" do
+        allow(packing_type).to receive(:activate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Packing type '#{packing_type.name}' could not be activated.")
+        expect(subject.payload[:packing_type]).to eq(packing_type)
+      end
+    end
+  end
 end
--- spec/services/packing_types/deactivate_service_spec.rb
+++ spec/services/packing_types/deactivate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe PackingTypes::DeactivateService, type: :service do
+  describe "#call" do
+    let(:packing_type) { create(:packing_type) }
+    subject { described_class.(packing_type) }
+
+    context "when deactivation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Packing type '#{packing_type.name}' was successfully deactivated.")
+        expect(subject.payload[:packing_type]).to eq(packing_type)
+      end
+    end
+
+    context "when deactivation fails" do
+      it "returns an error response" do
+        allow(packing_type).to receive(:deactivate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Packing type '#{packing_type.name}' could not be deactivated.")
+        expect(subject.payload[:packing_type]).to eq(packing_type)
+      end
+    end
+  end
 end
