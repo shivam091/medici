df77645dfd904e0d46745a646d473d383a396e8c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 12:52:24 +0530

    Added specs for `DosageForms::Activate` and `DosageForms::Deactivate` services

--- spec/services/dosage_forms/activate_service_spec.rb
+++ spec/services/dosage_forms/activate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe DosageForms::ActivateService, type: :service do
+  describe "#call" do
+    let(:dosage_form) { create(:dosage_form) }
+    subject { described_class.(dosage_form) }
+
+    context "when activation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Dosage form '#{dosage_form.name}' was successfully activated.")
+        expect(subject.payload[:dosage_form]).to eq(dosage_form)
+      end
+    end
+
+    context "when activation fails" do
+      it "returns an error response" do
+        allow(dosage_form).to receive(:activate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Dosage form '#{dosage_form.name}' could not be activated.")
+        expect(subject.payload[:dosage_form]).to eq(dosage_form)
+      end
+    end
+  end
 end
--- spec/services/dosage_forms/deactivate_service_spec.rb
+++ spec/services/dosage_forms/deactivate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe DosageForms::DeactivateService, type: :service do
+  describe "#call" do
+    let(:dosage_form) { create(:dosage_form) }
+    subject { described_class.(dosage_form) }
+
+    context "when deactivation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Dosage form '#{dosage_form.name}' was successfully deactivated.")
+        expect(subject.payload[:dosage_form]).to eq(dosage_form)
+      end
+    end
+
+    context "when deactivation fails" do
+      it "returns an error response" do
+        allow(dosage_form).to receive(:deactivate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Dosage form '#{dosage_form.name}' could not be deactivated.")
+        expect(subject.payload[:dosage_form]).to eq(dosage_form)
+      end
+    end
+  end
 end
