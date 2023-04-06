d4cb0988e97d883043aff8593848bc03993111e3
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 20:06:10 +0530

    Changes in shared examples

--- spec/support/shared_examples/returns_a_success_response.rb
+++ spec/support/shared_examples/returns_a_success_response.rb
@@ -4,6 +4,7 @@
 
 RSpec.shared_examples "returns a success response" do |klass|
   it "returns a success response" do
+    expect(subject).to be_a(ServiceResponse)
     expect(subject).to be_success
   end
 end
--- spec/support/shared_examples/returns_an_error_response.rb
+++ spec/support/shared_examples/returns_an_error_response.rb
@@ -4,6 +4,7 @@
 
 RSpec.shared_examples "returns an error response" do |klass|
   it "returns an error response" do
+    expect(subject).to be_a(ServiceResponse)
     expect(subject).to be_error
   end
 end
