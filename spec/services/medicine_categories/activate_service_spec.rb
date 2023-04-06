7a8b94b260e171c8a3ec0dd2eb3e2d3829edef9e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-06 12:52:49 +0530

    Added specs for `MedicineCategories::Activate` and `MedicineCategories::Deactivate` services

--- spec/services/medicine_categories/activate_service_spec.rb
+++ spec/services/medicine_categories/activate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe MedicineCategories::ActivateService, type: :service do
+  describe "#call" do
+    let(:medicine_category) { create(:medicine_category) }
+    subject { described_class.(medicine_category) }
+
+    context "when activation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Medicine category '#{medicine_category.name}' was successfully activated.")
+        expect(subject.payload[:medicine_category]).to eq(medicine_category)
+      end
+    end
+
+    context "when activation fails" do
+      it "returns an error response" do
+        allow(medicine_category).to receive(:activate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Medicine category '#{medicine_category.name}' could not be activated.")
+        expect(subject.payload[:medicine_category]).to eq(medicine_category)
+      end
+    end
+  end
 end
--- spec/services/medicine_categories/deactivate_service_spec.rb
+++ spec/services/medicine_categories/deactivate_service_spec.rb
@@ -7,4 +7,26 @@
 require "spec_helper"
 
 RSpec.describe MedicineCategories::DeactivateService, type: :service do
+  describe "#call" do
+    let(:medicine_category) { create(:medicine_category) }
+    subject { described_class.(medicine_category) }
+
+    context "when deactivation is successful" do
+      it "returns an success response" do
+        expect(subject).to be_success
+        expect(subject.message).to eq("Medicine category '#{medicine_category.name}' was successfully deactivated.")
+        expect(subject.payload[:medicine_category]).to eq(medicine_category)
+      end
+    end
+
+    context "when deactivation fails" do
+      it "returns an error response" do
+        allow(medicine_category).to receive(:deactivate!).and_return(false)
+
+        expect(subject).to be_error
+        expect(subject.message).to eq("Medicine category '#{medicine_category.name}' could not be deactivated.")
+        expect(subject.payload[:medicine_category]).to eq(medicine_category)
+      end
+    end
+  end
 end
