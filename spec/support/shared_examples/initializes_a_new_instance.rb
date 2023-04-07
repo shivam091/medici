e3d6ae1ca85a58bc612e06ffdb2fe880cc071b6d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-07 20:13:21 +0530

    Renamed shared example `assigns a new object` to `initializes a new instance`

--- spec/support/shared_examples/initializes_a_new_instance.rb
+++ spec/support/shared_examples/initializes_a_new_instance.rb
@@ -2,8 +2,8 @@
 # -*- frozen_string_literal: true -*-
 # -*- warn_indent: true -*-
 
-RSpec.shared_examples "assigns a new object" do |object_name, klass|
-  it "assigns a new #{klass} to @#{object_name}" do
+RSpec.shared_examples "initializes a new instance" do |object_name, klass|
+  it "initializes a new instance" do
     expect(ivar(object_name)).to be_a_new(klass)
   end
 end
