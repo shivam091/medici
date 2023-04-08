4a54a0646d31e7cb8913b2b93402dce3911758af
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-08 20:05:44 +0530

    Added specs for `Medicines::ActivateService` and `Medicines::DeactivateService`

--- spec/services/medicines/activate_service_spec.rb
+++ spec/services/medicines/activate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Medicines::ActivateService, type: :service do
+  describe "#call" do
+    let(:medicine) { create(:medicine, :with_user) }
+    subject { described_class.(medicine) }
+
+    context "when activation is successful" do
+      it "activates the medicine" do
+        expect { subject }.to change { medicine.reload.is_active? }.to(true)
+        expect(subject.message).to eq("Medicine '#{medicine.reference_code} (#{medicine.name})' was successfully activated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when activation fails" do
+      before { allow(medicine).to receive(:activate!).and_return(false) }
+
+      it "does not activate the medicine" do
+        expect { subject }.not_to change { medicine.reload.is_active? }
+        expect(subject.message).to eq("Medicine '#{medicine.reference_code} (#{medicine.name})' could not be activated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
--- spec/services/medicines/deactivate_service_spec.rb
+++ spec/services/medicines/deactivate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Medicines::DeactivateService, type: :service do
+  describe "#call" do
+    let(:medicine) { create(:medicine, :with_user, :active) }
+    subject { described_class.(medicine) }
+
+    context "when deactivation is successful" do
+      it "deactivates the medicine" do
+        expect { subject }.to change { medicine.reload.is_active? }.to(false)
+        expect(subject.message).to eq("Medicine '#{medicine.reference_code} (#{medicine.name})' was successfully deactivated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when deactivation fails" do
+      before { allow(medicine).to receive(:deactivate!).and_return(false) }
+
+      it "does not deactivate the medicine" do
+        expect { subject }.not_to change { medicine.reload.is_active? }
+        expect(subject.message).to eq("Medicine '#{medicine.reference_code} (#{medicine.name})' could not be deactivated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
