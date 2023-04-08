68c8c5c3c8c3c49c4e242b228cf53b364eb9412c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-08 20:05:52 +0530

    Added specs for `Stores::ActivateService` and `Stores::DeactivateService`

--- spec/services/stores/deactivate_service_spec.rb
+++ spec/services/stores/deactivate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Stores::DeactivateService, type: :service do
+  describe "#call" do
+    let(:store) { create(:store, :active) }
+    subject { described_class.(store) }
+
+    context "when deactivation is successful" do
+      it "deactivates the store" do
+        expect { subject }.to change { store.reload.is_active? }.to(false)
+        expect(subject.message).to eq("Store '#{store.name}' was successfully deactivated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when deactivation fails" do
+      before { allow(store).to receive(:deactivate!).and_return(false) }
+
+      it "does not deactivate the store" do
+        expect { subject }.not_to change { store.reload.is_active? }
+        expect(subject.message).to eq("Store '#{store.name}' could not be deactivated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
