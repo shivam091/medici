989f5ed27207afb97b9d4bdbdb78bd7289619d5a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-08 20:05:58 +0530

    Added specs for `Users::ActivateService` and `Users::DeactivateService`

--- spec/services/users/activate_service_spec.rb
+++ spec/services/users/activate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Users::ActivateService, type: :service do
+  describe "#call" do
+    let(:user) { create(:manager) }
+    subject { described_class.(user) }
+
+    context "when activation is successful" do
+      it "activates the user" do
+        expect { subject }.to change { user.reload.is_active? }.to(true)
+        expect(subject.message).to eq("User '#{user.full_name}' was successfully activated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when activation fails" do
+      before { allow(user).to receive(:activate!).and_return(false) }
+
+      it "does not activate the user" do
+        expect { subject }.not_to change { user.reload.is_active? }
+        expect(subject.message).to eq("User '#{user.full_name}' could not be activated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
--- spec/services/users/deactivate_service_spec.rb
+++ spec/services/users/deactivate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Users::DeactivateService, type: :service do
+  describe "#call" do
+    let(:user) { create(:manager, :active) }
+    subject { described_class.(user) }
+
+    context "when deactivation is successful" do
+      it "deactivates the user" do
+        expect { subject }.to change { user.reload.is_active? }.to(false)
+        expect(subject.message).to eq("User '#{user.full_name}' was successfully deactivated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when deactivation fails" do
+      before { allow(user).to receive(:deactivate!).and_return(false) }
+
+      it "does not deactivate the user" do
+        expect { subject }.not_to change { user.reload.is_active? }
+        expect(subject.message).to eq("User '#{user.full_name}' could not be deactivated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
