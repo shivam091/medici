ea8966837710ef945b214c32ccdba59714b7d16c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-08 19:56:13 +0530

    Added specs for `Manufacturers::ActivateService` and `Manufacturers::DeactivateService`

--- spec/services/manufacturers/activate_service_spec.rb
+++ spec/services/manufacturers/activate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Manufacturers::ActivateService, type: :service do
+  describe "#call" do
+    let(:manufacturer) { create(:manufacturer) }
+    subject { described_class.(manufacturer) }
+
+    context "when activation is successful" do
+      it "activates the manufacturer" do
+        expect { subject }.to change { manufacturer.reload.is_active? }.to(true)
+        expect(subject.message).to eq("Manufacturer '#{manufacturer.name}' was successfully activated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when activation fails" do
+      before { allow(manufacturer).to receive(:activate!).and_return(false) }
+
+      it "does not activate the manufacturer" do
+        expect { subject }.not_to change { manufacturer.reload.is_active? }
+        expect(subject.message).to eq("Manufacturer '#{manufacturer.name}' could not be activated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
--- spec/services/manufacturers/deactivate_service_spec.rb
+++ spec/services/manufacturers/deactivate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Manufacturers::DeactivateService, type: :service do
+  describe "#call" do
+    let(:manufacturer) { create(:manufacturer, :active) }
+    subject { described_class.(manufacturer) }
+
+    context "when deactivation is successful" do
+      it "deactivates the manufacturer" do
+        expect { subject }.to change { manufacturer.reload.is_active? }.to(false)
+        expect(subject.message).to eq("Manufacturer '#{manufacturer.name}' was successfully deactivated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when deactivation fails" do
+      before { allow(manufacturer).to receive(:deactivate!).and_return(false) }
+
+      it "does not deactivate the manufacturer" do
+        expect { subject }.not_to change { manufacturer.reload.is_active? }
+        expect(subject.message).to eq("Manufacturer '#{manufacturer.name}' could not be deactivated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
