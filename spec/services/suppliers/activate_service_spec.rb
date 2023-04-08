7ace498fde70cba634802658a68b00de7c14d560
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-08 19:56:25 +0530

    Added specs for `Suppliers::ActivateService` and `Suppliers::DeactivateService`

--- spec/services/suppliers/activate_service_spec.rb
+++ spec/services/suppliers/activate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Suppliers::ActivateService, type: :service do
+  describe "#call" do
+    let(:supplier) { create(:supplier) }
+    subject { described_class.(supplier) }
+
+    context "when activation is successful" do
+      it "activates the supplier" do
+        expect { subject }.to change { supplier.reload.is_active? }.to(true)
+        expect(subject.message).to eq("Supplier '#{supplier.name}' was successfully activated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when activation fails" do
+      before { allow(supplier).to receive(:activate!).and_return(false) }
+
+      it "does not activate the supplier" do
+        expect { subject }.not_to change { supplier.reload.is_active? }
+        expect(subject.message).to eq("Supplier '#{supplier.name}' could not be activated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
--- spec/services/suppliers/deactivate_service_spec.rb
+++ spec/services/suppliers/deactivate_service_spec.rb
@@ -7,4 +7,28 @@
 require "spec_helper"
 
 RSpec.describe Suppliers::DeactivateService, type: :service do
+  describe "#call" do
+    let(:supplier) { create(:supplier, :active) }
+    subject { described_class.(supplier) }
+
+    context "when deactivation is successful" do
+      it "deactivates the supplier" do
+        expect { subject }.to change { supplier.reload.is_active? }.to(false)
+        expect(subject.message).to eq("Supplier '#{supplier.name}' was successfully deactivated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when deactivation fails" do
+      before { allow(supplier).to receive(:deactivate!).and_return(false) }
+
+      it "does not deactivate the supplier" do
+        expect { subject }.not_to change { supplier.reload.is_active? }
+        expect(subject.message).to eq("Supplier '#{supplier.name}' could not be deactivated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
