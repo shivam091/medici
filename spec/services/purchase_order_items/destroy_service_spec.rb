b8a3fd352da852af8edfbbe7ff2e26601bd55181
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-09 19:52:28 +0530

    Added service to `destroy` the `purchase order item`

---
+++ spec/services/purchase_order_items/destroy_service_spec.rb
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/purchase_order_items/destroy_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PurchaseOrderItems::DestroyService, type: :service do
+  context "doing something" do
+    it "should test something" do
+      skip
+    end
+  end
+end
