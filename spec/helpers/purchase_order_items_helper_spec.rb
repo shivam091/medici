f7f0c01ee85170dbf7544395e332a6fc4b8453e1
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-09 19:52:45 +0530

    Added helper to resolve routes of `purchase order items`

---
+++ spec/helpers/purchase_order_items_helper_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/helpers/purchase_order_items_helper_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PurchaseOrderItemsHelper, type: :helper do
+end
